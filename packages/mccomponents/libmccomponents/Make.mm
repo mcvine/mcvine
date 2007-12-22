49cdc37914c0df429a3fae7290e411ccb31cceeb
Jiao Lin <linjiao@caltech.edu>
2007-12-22 21:50:59 +0000

    directory skeleton of "mccomponents"
    git-svn-id: svn://danse.us/MCViNE/trunk@36 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mccomponents/libmccomponents/Make.mm
@@ -0,0 +1,52 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2005  All Rights Reserved
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+include local.def
+
+PROJECT = mccomponents
+PACKAGE = libmccomponents
+
+
+# directory structure
+
+BUILD_DIRS = \
+	headers \
+	mccomponents \
+	sharedlib \
+
+OTHER_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
+
+
+#--------------------------------------------------------------------------
+# build the library
+all: 
+	BLD_ACTION="all" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+#--------------------------------------------------------------------------
+
+
+
+# version
+# $Id$
+
+#
+# End of file
