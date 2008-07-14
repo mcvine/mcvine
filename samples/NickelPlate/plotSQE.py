beb42d599314f36fb7808c7b1a605ef6faf28b37
Jiao Lin <linjiao@caltech.edu>
2008-07-14 16:55:12 +0000

    minor restructure
    git-svn-id: svn://danse.us/MCViNE/trunk@213 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- samples/NickelPlate/plotSQE.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-path = sys.argv[1]
-from mccomponents.sample.idf  import readSQE
-sqe = readSQE( path )
-from histogram.plotter import defaultPlotter
-defaultPlotter.plot( sqe )
