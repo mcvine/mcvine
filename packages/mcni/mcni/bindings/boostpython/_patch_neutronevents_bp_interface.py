366e01dea7d200ee3ac6e5854dba4a7e9c7c14b2
Jiao Lin <linjiao@caltech.edu>
2008-07-20 02:41:54 +0000

    minor: string representation of neutron improved.
    git-svn-id: svn://danse.us/MCViNE/trunk@238 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mcni/mcni/bindings/boostpython/_patch_neutronevents_bp_interface.py
+++ packages/mcni/mcni/bindings/boostpython/_patch_neutronevents_bp_interface.py
@@ -7,7 +7,7 @@ from mcni.mcnibp import vector_Event
 original__str__ = vector_Event.__str__
 def __str__(self):
     if len(self)>10: return original__str__(self)
-    return ','.join( [ str(n) for n in self ] )
+    return ', '.join( [ '%s' % n for n in self ] )
 
 vector_Event.__str__ = __str__
     
