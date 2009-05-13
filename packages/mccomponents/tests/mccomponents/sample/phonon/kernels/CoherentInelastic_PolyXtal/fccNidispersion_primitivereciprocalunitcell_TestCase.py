fbf0c8de03ffef204d8bd5af606511d298bada8a
Jiao Lin <linjiao@caltech.edu>
2009-05-13 17:48:29 +0000

    CoherentInelastic_PolyXtal: added a small test case for idf dispersion
    git-svn-id: svn://danse.us/MCViNE/trunk@283 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mccomponents/tests/mccomponents/sample/phonon/kernels/CoherentInelastic_PolyXtal/fccNidispersion_primitivereciprocalunitcell_TestCase.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2009 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+'''
+'''
+
+
+import unittestX as unittest
+import journal
+
+
+class TestCase(unittest.TestCase):
+
+    def test(self):
+        "fcc Ni dispersion read from idf data directory: primitive reciprocal unitcell"
+        disp = makeDispersion()
+        Q = bp.vector3
+        a = 3.57 # angstrom
+        b = 2*N.pi/a
+        print disp.energy(0, Q(0,0,0))
+        for i in range(3):
+            print disp.energy(i, Q(b*1.05,b*1.05,b*1.05))
+        return
+
+    pass  # end of TestCase
+
+
+
+def makeDispersion():
+    from mccomponents.sample.phonon import periodicdispersion_fromidf
+    disp = periodicdispersion_fromidf('phonon-dispersion-fccNi-primitive-reciprocal-unitcell')
+    from mccomponents.homogeneous_scatterer import kernelEngine, scattererEngine
+    disp = scattererEngine(disp)
+    return disp
+
+
+import numpy as N
+import mccomponents.sample.phonon.bindings as bindings
+bp = bindings.get('BoostPython')
+
+
+def pysuite():
+    suite1 = unittest.makeSuite(TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+
+def main():
+    #debug.activate()
+    journal.debug('periodicdispersion_3d').activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    unittest.TextTestRunner(verbosity=2).run(alltests)
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id$"
+
+# End of file 
