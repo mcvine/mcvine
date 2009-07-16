a5dda76d9fc9a8ccacc94f2595559df8bf84cd99
Alta Fang <alta.and.ebie@gmail.com>
2009-07-16 17:47:44 +0000

    Tiny fix on raising exceptions
    git-svn-id: svn://danse.us/MCViNE/trunk@311 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/legacycomponents/mcstas2/mcstas2/wrappers/binding_builder/mm/utils.py
+++ packages/legacycomponents/mcstas2/mcstas2/wrappers/binding_builder/mm/utils.py
@@ -16,7 +16,7 @@ def callmm( path ):
     import os
     curdir = os.path.abspath( os.curdir )
     os.chdir( path )
-    if os.system( 'mm' ): raise "mm failed in %s" % path
+    if os.system( 'mm' ): raise Exception("mm failed in %s" % path)
     os.chdir( curdir )
     return
     
