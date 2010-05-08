70c058b3e1a78da7432e6f7a777da79d029bc1c4
Jiao Lin <linjiao@caltech.edu>
2010-05-08 05:53:21 +0000

    mccomponents/tests/mccomponents/sample/samplecomponent_IsotropicKernel_multiplescattering_TestCase.py: added
    git-svn-id: svn://danse.us/MCViNE/trunk@347 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mccomponents/tests/mccomponents/sample/samplecomponent_IsotropicKernel_multiplescattering_TestCase.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2007 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+
+import unittestX as unittest
+import journal
+
+debug = journal.debug( "samplecomponent_TestCase" )
+warning = journal.warning( "samplecomponent_TestCase" )
+
+
+scattererxml = 'Ni-scatterer.xml'
+
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        'mccomponents.sample.samplecomponent'
+        import mcni
+        neutron = mcni.neutron( r = (0,0,0), v = (0,0,3000), time = 0, prob = 1 )
+        from mcni.components.MonochromaticSource import MonochromaticSource
+        component1 = MonochromaticSource('source', neutron)
+        from mccomponents.sample import samplecomponent
+        component2 = samplecomponent( 'Al', 'sampleassembly/Al-isotropickernel.xml' )
+        instrument = mcni.instrument( [component1, component2] )
+        
+        geometer = mcni.geometer()
+        geometer.register( component1, (0,0,0), (0,0,0) )
+        geometer.register( component2, (0,0,1), (0,0,0) )
+
+        N0 = 1
+        neutrons = mcni.neutron_buffer(N0)
+
+        mcni.simulate( instrument, geometer, neutrons, multiple_scattering=True)
+
+        N = len(neutrons)
+
+        for i in range(N):
+            neutron = neutrons[i]
+            print neutron
+            continue
+
+        return
+    
+
+    pass  # end of TestCase
+
+
+def main():
+    unittest.main()
+    return
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id$"
+
+# End of file 
