bb79c139df4b329ab4fc8abddcd49954d19f8dd7
Jiao Lin <linjiao@caltech.edu>
2010-05-20 05:06:24 +0000

    legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/IQE_monitor.py: normalize intensity by solid angle and (kf/ki) so it can be compared to S(Q,E)
    git-svn-id: svn://danse.us/MCViNE/trunk@364 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/IQE_monitor.py
+++ packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/IQE_monitor.py
@@ -19,9 +19,15 @@ class ComponentInterface(base):
 
     def _get_histogram(self):
         return get_histogram(self)
+
+
+def get_histogram(monitor):
+    rh = get_raw_histogram(monitor)
+    norm = getNormalization(monitor, N=1000000)
+    return rh/norm
     
-    
-def get_histogram( monitor ):
+
+def get_raw_histogram( monitor ):
     from mcstas2.utils.carray import bpptr2npyarr
     core = monitor.core()
 
@@ -42,6 +48,78 @@ def get_histogram( monitor ):
 
     h = histogram( 'I(Q,E)', [Qaxis,Eaxis], data = Iarr, errors = E2arr )
     return h
+
+
+def getNormalization(monitor, N=100000):
+    # randomly shoot neutrons to monitor in 4pi solid angle
+    
+    core = monitor.core()
+    import mcni, random, mcni.utils.conversion as conversion, math, os
+    
+    # 1. create neutrons
+    neutrons = mcni.neutron_buffer(N)
+    vi = conversion.e2v(core.Ei)
+    for i in range(N):
+        # randomly select E, the energy transfer
+        E = core.Emin + random.random() * (core.Emax-core.Emin)
+        # the final energy
+        Ef = core.Ei - E
+        # the final velocity
+        vf = conversion.e2v(Ef)
+        # choose theta
+        theta = random.random() * math.pi
+        # choose phi
+        phi = random.random() * math.pi * 2.
+        # compute final velocity vector
+        vfv = vf*math.sin(theta)*math.cos(phi), vf*math.sin(theta)*math.sin(phi), vf*math.cos(theta)
+        # prob
+        prob = math.sin(theta) * (vf/vi)
+        # the neutron
+        neutrons[i] = mcni.neutron(prob=prob, v=vfv)
+        continue
+    
+    # 2. create a copy of the original monitor
+    from mcstas2 import componentfactory
+    cf = componentfactory(type='IQE_monitor', category='monitors')
+    props = [
+        'Ei',
+        'Emin', 'Emax', 'nE',
+        'Qmin', 'Qmax', 'nQ',
+        'max_angle_out_of_plane', 'min_angle_out_of_plane',
+        'max_angle_in_plane', 'min_angle_in_plane',
+        ]
+    kwds = {}
+    for p in props: kwds[p] = getattr(core, p)
+    import tempfile
+    tmpdir = tempfile.mkdtemp()
+    outfilename = os.path.join(tmpdir, 'mon.dat')
+    kwds['filename'] = outfilename
+    monitorcopy = cf('monitor', **kwds)
+    
+    # 3. send neutrons to monitor copy
+    monitorcopy.process(neutrons)
+    h = get_raw_histogram(monitorcopy)
+    # for debug
+    # import histogram.hdf as hh
+    # hh.dump(h, 'tmp.h5', '/', 'c')
+    h.I[h.I==0] = 1
+    return h
+
+
+def test1():
+    from mcstas2 import componentfactory
+    cf = componentfactory(type='IQE_monitor', category='monitors')
+    monitor = cf('monitor')
+    getNormalization(monitor, N=1000000)
+    return
+
+
+def main():
+    test1()
+    return
+
+
+if __name__ == '__main__': main()
 
 
 # version
