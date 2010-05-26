7ea9b21a6fdce1f8433583b415025347a5c913b5
Jiao Lin <linjiao@caltech.edu>
2010-05-26 18:42:11 +0000

    tests/math/test_fparser.cc: added a test case for f(Q,E)
    git-svn-id: svn://danse.us/MCViNE/trunk@402 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/tests/libmccomponents/math/test_fparser.cc
+++ packages/mccomponents/tests/libmccomponents/math/test_fparser.cc
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <cassert>
 #include <string>
+#include <cmath>
 #include "fparser/fparser.hh"
 
 
@@ -22,7 +23,8 @@ int test1()
   std::string function = "sin(2*pi*x)";
   FunctionParser fparser;
   
-  fparser.AddConstant("pi", 3.1415926535897932);
+  double pi = 3.1415926535897932;
+  fparser.AddConstant("pi", pi);
 
   int res = fparser.Parse(function, "x");
   if (res>=0) {
@@ -33,14 +35,43 @@ int test1()
   for (double x=0; x<1; x+=0.25) {
     vals[0] = x;
     double y = fparser.Eval(vals);
-    std::cout << "f(" << x << ") = " << y << std::endl;
+    // std::cout << "f(" << x << ") = " << y << std::endl;
+    assert (y==std::sin(2*pi*x));
   }
+  return 0;
+}
+
+
+int test2()
+{
+  std::string function = "Q+E", vars="Q,E";
+  FunctionParser fparser;
+  
+  fparser.AddConstant("pi", 3.1415926535897932);
+
+  int res = fparser.Parse(function, vars);
+  if (res>=0) {
+    std::cout << "failed to parse function " << function << " with vars " << vars << std::endl;
+    return 1;
+  }
+  
+  double vals[] = {0,0};
+  for (double Q=0; Q<10; Q+=0.1) {
+    for (double E=0; E<40; E+=0.5) {
+      vals[0] = Q; vals[1] = E;
+      double y = fparser.Eval(vals);
+      // std::cout << "f(" << Q << "," << E << ") = " << y << std::endl;
+      assert (std::abs(y-(Q+E)) < 1e-6);
+    }
+  }
+  return 0;
 }
 
 
 int main()
 {
-  test1();
+  if (test1()) return 1;
+  if (test2()) return 1;
   return 0;
 }
 
