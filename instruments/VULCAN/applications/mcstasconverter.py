e9b5df4c6d57a60af96d051fac354740cdde57fb
Alexander Dementsov <dexity@gmail.com>
2010-08-20 17:58:34 +0000

    Added stub file for mcstas converter to mcvine
    git-svn-id: svn://danse.us/MCViNE/trunk@442 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ instruments/VULCAN/applications/mcstasconverter.py
@@ -0,0 +1,27 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2010  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+"""
+McStasConverter - converts McStas component to the McVine component
+
+
+"""
+
+class McStasConverter:
+
+    def __init__(self, filename):
+        pass
+
+__date__ = "$Aug 19, 2010 10:25:18 AM$"
+
+
