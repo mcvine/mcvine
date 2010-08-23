39f27a72da4bf42b78ed0ad2b0854c330f7ab4b3
Alexander Dementsov <dexity@gmail.com>
2010-08-23 14:16:35 +0000

    Created reg ex for component with parameters
    git-svn-id: svn://danse.us/MCViNE/trunk@444 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- instruments/VULCAN/applications/mcstasconverter.py
+++ instruments/VULCAN/applications/mcstasconverter.py
@@ -38,13 +38,29 @@ is converted to:
 		"Lmax": "14.0",
 		"restore_neutron": "1"}
 
-
+Issues:
+    - Check if name is case sensitive (like COMPONENT)
+    - Extract properties from "extra" to separate properties
 """
 
 # XXX: Figure out what other metadata to extract from "extra" properties
+
+tempText = """COMPONENT L_monitor9 = L_monitor(
+    nchan = 140, filename = "Vulcan_asbuilt_L_monitor9.txt",
+    xwidth = 0.15, yheight = 0.15, Lmin = 0.0, Lmax = 14.0,
+    restore_neutron = 1)
+  AT (0, 0, 0.971)  RELATIVE  FU_Out"""
+
+
+
 # Regular expressions
+SPACES          = '[ \t]*'              # Spaces and tabs
+NAME            = '%s([^ ()=]*)%s' % (SPACES, SPACES)  # Extracts name
+NO_BRACKETS     = '[^()]*'              # No brackets
+PARAMETERS      = '(%s)' % NO_BRACKETS  # Component parameters
+COMPONENT       = "COMPONENT%s=%s\(%s\)" %(NAME, NAME, PARAMETERS)  # Component
 
-
+import re
 
 class McStasConverter:
 
@@ -53,12 +69,25 @@ class McStasConverter:
         self._components    = []    # list of dictionaries
 
     def parse(self):
-        pass
+        str = "COMPONENT L_monitor9 = L_monitor(\nhello)"
+        p   = re.compile(COMPONENT)
+        matches     = p.findall(tempText)        # Finds all components
+
+        print matches
+        
+#        for m in matches:
+#            name    = m[0].lower()
+#            print name
 
 
+    # XXX: Fix
     def toString(self):
-        pass
+        print self._components
 
+
+if __name__ == "__main__":
+    conv    = McStasConverter("hi")
+    conv.parse()
 
 __date__ = "$Aug 19, 2010 10:25:18 AM$"
 
