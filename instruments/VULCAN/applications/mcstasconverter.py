7c1d0d22dcb2bd9ff35ae1e603744a4c81eadf09
Alexander Dementsov <dexity@gmail.com>
2010-08-23 19:22:12 +0000

    Fixed issue with parsing ROTATED
    git-svn-id: svn://danse.us/MCViNE/trunk@446 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- instruments/VULCAN/applications/mcstasconverter.py
+++ instruments/VULCAN/applications/mcstasconverter.py
@@ -23,12 +23,14 @@ COMPONENT L_monitor9 = L_monitor(
     xwidth = 0.15, yheight = 0.15, Lmin = 0.0, Lmax = 14.0,
     restore_neutron = 1)
   AT (0, 0, 0.971)  RELATIVE  FU_Out
+  ROTATED (0,ROT,0) relative arm
 
 is converted to:
 
 {"name":        "L_monitor9",
 "type":         "L_monitor",
 "position":     "AT (0, 0, 0.971)  RELATIVE  FU_Out",
+"rotation":     "ROTATED (0,ROT,0) relative arm",
 "extra":        [], # like ROTATION
 "parameters": {	"nchan": "140",
 		"filename": "'Vulcan_asbuilt_L_monitor9.txt'",
@@ -51,7 +53,7 @@ tempText = """COMPONENT L_monitor9 = L_monitor(
     xwidth = 0.15, yheight = 0.15, Lmin = 0.0, Lmax = 14.0,
     restore_neutron = 1)
   AT (0, 0, 0.971)  RELATIVE  FU_Out
-  AT (0, 0, 0.971)  RELATIVE  FU_Out
+  ROTATED (0,ROT,0) relative arm
 
   """
 
@@ -79,19 +81,20 @@ class McStasConverter:
         str2    = "restore_neutron = 1)\n  AT (0, 0, 0.971)  RELATIVE  FU_Out\n\n"
         str3    = """COMPONENT L_monitor9 = L_monitor(restore_neutron = 1)
   AT (0, 0, 0.971)  RELATIVE  FU_Out
-  AT (0, 0, 0.971)  RELATIVE  FU_Out
+  ROTATED (0,ROT,0) relative arm
 
 """
         # Remove comments
         p   = re.compile(COMPONENT, re.DOTALL)
-        matches     = p.findall(tempText)       # Finds all components
+        matches     = p.findall(str3)   #tempText)       # Finds all components
 
         for m in matches:
             comp    = {}
             comp["name"]        = m[0]
             comp["type"]        = m[1]
-            comp["position"]    = self._position(m[3])
             comp["parameters"]  = self._params(m[2])
+            comp["position"]    = self._position(m[3])
+            comp["rotation"]    = self._rotation(m[3])
 
             self._components.append(comp)
 
@@ -101,7 +104,7 @@ class McStasConverter:
         if not text:
             return {}   # Empty dictionary
 
-        text    = """\n    nchan = 140, filename = "Vulcan_asbuilt_L_monitor9.txt",\n    xwidth = 0.15, yheight = 0.15, Lmin = 0.0, Lmax = 14.0,\n    restore_neutron = 1"""
+        #text    = """\n    nchan = 140, filename = "Vulcan_asbuilt_L_monitor9.txt",\n    xwidth = 0.15, yheight = 0.15, Lmin = 0.0, Lmax = 14.0,\n    restore_neutron = 1"""
         plist   = text.split(",")
         params  = {}
         for pp in plist:
@@ -114,29 +117,40 @@ class McStasConverter:
         return params
 
 
-    def _position(self, text):
-        "Extracts position from text"
-        if not text:
-            return ""
-
-        ilist       = text.split("\n")
-        position    = ""
-        for i in ilist:
-            str = i.strip("\n")
-            if str == "":   # get rid of empty line
-                continue
-
-            if len(str.split("AT")) != 1:   # 'AT' is in the string
-                position    = str.strip()   # If more than one 'AT' is set, take the last
-
-        return position
-
-
     def components(self):
         "Returns list of components"
         return self._components
 
-        
+
+    def _position(self, text):
+        "Extracts position from text"
+        return self._property("AT", text)
+
+
+    def _rotation(self, text):
+        "Extracts rotation from text"
+        return self._property("ROTATED", text)
+
+
+
+    def _property(self, key, text):
+        "Takes key and extracts pproperty from text"
+        if not key or not text:
+            return ""
+
+        ilist       = text.split("\n")
+        property    = ""
+        for i in ilist:
+            str = i.strip("\n ")
+            if str == "":   # get rid of empty line
+                continue
+
+            if str.startswith("%s " % key.upper()): # E.g. starts with "AT "
+                property    = str                   # If more than one key is set, take the last one
+
+        return property
+
+
     # XXX: Fix
     def toString(self, indent=4, br="\n"):
         #print self._params("hi")
