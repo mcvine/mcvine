b3cb0009c6d5e0f18498c26f19491d01e58b264a
Alexander Dementsov <dexity@gmail.com>
2010-08-23 03:49:11 +0000

    Defined format for mcstas converter. Added test case and file fixture for unit test
    git-svn-id: svn://danse.us/MCViNE/trunk@443 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- instruments/VULCAN/applications/mcstasconverter.py
+++ instruments/VULCAN/applications/mcstasconverter.py
@@ -14,13 +14,51 @@
 """
 McStasConverter - converts McStas component to the McVine component
 
+Example:
+
+McStas component:
+
+COMPONENT L_monitor9 = L_monitor(
+    nchan = 140, filename = "Vulcan_asbuilt_L_monitor9.txt",
+    xwidth = 0.15, yheight = 0.15, Lmin = 0.0, Lmax = 14.0,
+    restore_neutron = 1)
+  AT (0, 0, 0.971)  RELATIVE  FU_Out
+
+is converted to:
+
+{"name":        "L_monitor9",
+"type":         "L_monitor",
+"position":     "AT (0, 0, 0.971)  RELATIVE  FU_Out",
+"extra":        [], # like ROTATION
+"parameters": {	"nchan": "140",
+		"filename": "'Vulcan_asbuilt_L_monitor9.txt'",
+		"xwidth": "0.15",
+		"yheight": "0.15",
+		"Lmin": "0.0",
+		"Lmax": "14.0",
+		"restore_neutron": "1"}
+
 
 """
+
+# XXX: Figure out what other metadata to extract from "extra" properties
+# Regular expressions
+
+
 
 class McStasConverter:
 
     def __init__(self, filename):
+        self._filename      = filename
+        self._components    = []    # list of dictionaries
+
+    def parse(self):
         pass
+
+
+    def toString(self):
+        pass
+
 
 __date__ = "$Aug 19, 2010 10:25:18 AM$"
 
