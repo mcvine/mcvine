9ab104412121ae0036a28e8799d12aee567df854
Alexander Dementsov <dexity@gmail.com>
2010-08-24 03:29:25 +0000

    Implemented unit tests for mcstas converter and fixed some issues
    git-svn-id: svn://danse.us/MCViNE/trunk@454 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ instruments/VULCAN/tests/fixtures.py
@@ -0,0 +1,25 @@
+
+
+textMain = """
+/* Here, a secondary arm - or reference point, placed  */
+/* on the sample position. The ROT parameter above     */
+
+COMPONENT L_monitor9 = L_monitor(
+    nchan = 140, filename = "Vulcan_asbuilt_L_monitor9.txt",
+    xwidth = 0.15, yheight = 0.15, Lmin = 0.0, Lmax = 14.0,
+    restore_neutron = 1)
+  AT (0, 0, 0.971)  RELATIVE  FU_Out
+  ROTATED (0,ROT,0) relative arm
+
+COMPONENT arm2 = Arm()
+  AT (0,0,0) RELATIVE target
+  ROTATED (0,ROT,0) relative arm
+
+FINALLY
+%{
+%}
+/* The END token marks the instrument definition end */
+END
+
+
+  """
--- instruments/VULCAN/tests/mcstas_converter_TestCase.py
+++ instruments/VULCAN/tests/mcstas_converter_TestCase.py
@@ -11,6 +11,77 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+import unittest
+from mcstasconverter import McStasConverter
+import fixtures
+
+class mcstas_converter_TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.conv    = McStasConverter(filename="vanadium_example.instr")
+
+
+    def test_components_num(self):
+        components  = self.conv.components()
+        self.assertEqual(len(components), 6)
+
+
+    def test_component_first(self):
+        first   = self.conv.components()[0]
+        self.assertEqual(first["name"], "arm")
+        self.assertEqual(first["type"], "Arm")
+        self.assertEqual(first["position"], "AT (0,0,0) ABSOLUTE")
+        self.assertEqual(first["rotation"], "")
+        self.assertEqual(first["extra"], [])
+        self.assertEqual(first["parameters"], {})
+
+
+    def test_component_rotation(self):
+        five    = self.conv.components()[4]
+        self.assertEqual(five["position"], "AT (0,0,0) RELATIVE target")
+        self.assertEqual(five["rotation"], "ROTATED (0,ROT,0) relative arm")
+
+        
+    def test_component_params(self):
+        six     = self.conv.components()[5]
+        params  = six["parameters"]
+        self.assertEqual(len(params), 4)
+        self.assertEqual(params["nx"], "101")
+        self.assertEqual(params["ny"], "51")
+        self.assertEqual(params["radius"], "10")
+        self.assertEqual(params["filename"], '"vanadium.psd"')
+
+
+class mcstas_converter_config_TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.conv    = McStasConverter(config=fixtures.textMain)
+
+    def test_component_num(self):
+        self.assertEqual(len(self.conv.components()), 2)
+        
+
+    def test_component_first(self):
+        first   = self.conv.components()[0]
+        self.assertEqual(first["name"], "L_monitor9")
+        self.assertEqual(first["type"], "L_monitor")
+        self.assertEqual(first["position"], "AT (0, 0, 0.971)  RELATIVE  FU_Out")
+        self.assertEqual(first["rotation"], "ROTATED (0,ROT,0) relative arm")
+        self.assertEqual(first["extra"], [])
+
+
+    def test_component_params(self):
+        # Test some parameters
+        first   = self.conv.components()[0]
+        params  = first["parameters"]
+        self.assertEqual(len(params), 7)
+        self.assertEqual(params["nchan"], "140")
+        self.assertEqual(params["yheight"], "0.15")
+        self.assertEqual(params["filename"], '"Vulcan_asbuilt_L_monitor9.txt"')
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 __date__ = "$Aug 22, 2010 8:45:09 PM$"
 
