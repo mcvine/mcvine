908e72304355bb093c897233cd5919ee08435227
Jiao Lin <linjiao@caltech.edu>
2010-09-09 19:48:30 +0000

    mcstas2: TOF_monitor2 fixed a bug found by Doug
    git-svn-id: svn://danse.us/MCViNE/trunk@489 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/TOF_monitor2.py
+++ packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/TOF_monitor2.py
@@ -29,7 +29,7 @@ def get_histogram( monitor ):
     E2arr = bpptr2npyarr( core.getTOF_p2( ), 'double', n ).copy()
     from histogram import histogram, axis, arange
     dt = (core.tmax-core.tmin)/core.nchan
-    taxis = axis( 'tof', arange( core.tmin, core.tmax-0.1*dt, dt ), unit = 's' )
+    taxis = axis( 'tof', arange( core.tmin+dt/2., core.tmax+dt/2-0.1*dt, dt ), unit = 's' )
     h = histogram( 'I(tof)', [taxis], data = Iarr, errors = E2arr )
     return h
 
