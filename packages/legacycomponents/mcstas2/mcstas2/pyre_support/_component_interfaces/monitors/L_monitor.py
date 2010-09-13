e4a523cf2393d7745c9e7ad9020753af80f2f32f
Jiao Lin <linjiao@caltech.edu>
2010-09-13 17:08:47 +0000

    monitors/L_monitor.py: minor cosmetic changes
    git-svn-id: svn://danse.us/MCViNE/trunk@497 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/L_monitor.py
+++ packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/L_monitor.py
@@ -1,20 +1,22 @@
-# -*- Python -*-
+#!/usr/bin/env python
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 #                               Alex Dementsov
+#                                   Jiao Lin
 #                      California Institute of Technology
-#                        (C) 2010  All Rights Reserved
+#                      (C) 2006-2010  All Rights Reserved
 #
 # {LicenseText}
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+
 from default import ComponentInterface as base
 
 class ComponentInterface(base):
-
+    
     def _get_histogram( self ):
         return get_histogram(self)
 
@@ -24,13 +26,15 @@ def get_histogram(monitor):
     core = monitor.core()
     n = core.nchan
     Iarr = bpptr2npyarr( core.getL_p( ), 'double', n ).copy()
-    L2arr = bpptr2npyarr( core.getL_p2( ), 'double', n ).copy()
+    E2arr = bpptr2npyarr( core.getL_p2( ), 'double', n ).copy()
     from histogram import histogram, axis, arange
-    dL = (core.Lmax-core.Lmin)/core.nchan
+    dL = (core.Lmax-core.Lmin)/core.nchan 
     Laxis = axis( 'wavelength', arange( core.Lmin+dL/2, core.Lmax+dL/2, dL ), unit = 'angstrom' )
-    h = histogram( 'I(L)', [Laxis], data = Iarr, errors = L2arr )
+    h = histogram( 'I(L)', [Laxis], data = Iarr, errors = E2arr )
     return h
 
-__date__ = "$Sep 12, 2010 7:28:59 PM$"
 
+# version
+__id__ = "$Id$"
 
+# End of file 
