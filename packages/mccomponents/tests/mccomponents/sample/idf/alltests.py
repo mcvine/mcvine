7275f36e4e0654f855c3ad3b760adec97d59aec1
Alexander Dementsov <dexity@gmail.com>
2010-10-13 20:06:32 +0000

    Included read-table-lib to Guide_gravity
    git-svn-id: svn://danse.us/MCViNE/trunk@623 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/tests/mccomponents/sample/idf/alltests.py
+++ packages/mccomponents/tests/mccomponents/sample/idf/alltests.py
@@ -49,6 +49,6 @@ if __name__ == "__main__": main()
 
 
 # version
-__id__ = "$Id: alltests.py 947 2006-05-31 01:51:51Z jiao $"
+__id__ = "$Id: alltests.py 601 2010-10-03 19:55:29Z linjiao $"
 
 # End of file 
