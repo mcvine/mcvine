43462382bd95f998a9e4a4f4eb04fe039faa33f3
Alexander Dementsov <dexity@gmail.com>
2010-10-14 03:05:18 +0000

    Improved toInstrString()
    git-svn-id: svn://danse.us/MCViNE/trunk@628 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ instruments/VULCAN/applications/mcstascomponent.py
@@ -0,0 +1,21 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2010  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+# Keeps fields about McStas component
+
+class McstasComponent:
+    pass
+
+__date__ = "$Oct 13, 2010 7:18:49 PM$"
+
+
