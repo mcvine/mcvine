c47c29e67019bffd495548a6d7757a3e683c46a2
Jiao Lin <linjiao@caltech.edu>
2010-10-21 22:57:42 +0000

    geometry/test_choose_direction.cc: add a test that checks isotropicity of 4pi version of choose_direction function
    git-svn-id: svn://danse.us/MCViNE/trunk@648 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/tests/libmccomponents/math/geometry/test_choose_direction.cc
+++ packages/mccomponents/tests/libmccomponents/math/geometry/test_choose_direction.cc
@@ -24,14 +24,54 @@ using namespace mccomponents::math;
 char * jrnltag ="test_choose_direction";
 
 typedef mcni::Vector3<double> V3;
+using mcni::PI;
 
 
 void test1()
 {
-  int N = 10000;
   V3 dir;
+  double theta, phi;
+
+  const int Ntheta = 10, Nphi = 10;
+  const int N1 = 10000;
+  const int N = Ntheta*Nphi*N1;
+
+  double I[Ntheta*Nphi];
+  int itheta, iphi;
+  
   for (int i=0; i<N; i++) {
     choose_direction(dir);
+    mcni::assertAlmostEqual(dir.length(), 1.);
+    
+    theta = std::acos(dir.z);
+    phi = std::atan2(dir.y, dir.x);
+    
+    // convert to cos(theta) because solid angle is sintheta dtheta dphi = d(costheta) dphi
+    itheta = (std::cos(theta)+1)/2*Ntheta;
+    iphi = (phi+PI)/2/PI * Nphi;
+
+    /*
+    std::cout << "theta=" << theta << ", "
+	      << "phi = " << phi 
+	      << std::endl;
+    */
+    I[itheta*Nphi + iphi] += 1;
+  }
+
+  double sigma = sqrt(N1);
+  for (int i=0; i<Ntheta*Nphi; i++) {
+    /*
+    std::cout << "i=" << i << ", "
+	      << "I=" << I[i] 
+	      << std::endl;
+    */
+    if (isnan(I[i]))
+      std::cout 
+	<< "**Warning: "
+	<< "i=" << i << ", "
+	<< "I=" << I[i] 
+	<< std::endl;
+    mcni::assertAlmostEqual(I[i], N1, 3./sigma, 3*sigma);
   }
 }
 
