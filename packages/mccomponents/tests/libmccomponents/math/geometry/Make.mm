799a7be6895c944edc8ae66da27096eb0d1c5906
Jiao Lin <linjiao@caltech.edu>
2010-10-21 22:20:16 +0000

    starting to add test for "choose_direction"
    git-svn-id: svn://danse.us/MCViNE/trunk@646 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/tests/libmccomponents/math/geometry/Make.mm
+++ packages/mccomponents/tests/libmccomponents/math/geometry/Make.mm
@@ -10,35 +10,22 @@
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-PROJECT = mccomponents
+PROJECT = mccomposite
 PACKAGE = tests
 
-PROJ_CLEAN += $(PROJ_CPPTESTS)
 PROJ_TIDY += $(PROJ_CPPTESTS)
+PROJ_CLEAN += $(PROJ_CPPTESTS)
 
 PROJ_PYTESTS =  alltests.py
-PROJ_CPPTESTS = \
-	test_fparser \
-	test_random \
-	test_rootfinding \
-
+PROJ_CPPTESTS = test_choose_direction
 PROJ_TESTS = $(PROJ_PYTESTS) $(PROJ_CPPTESTS)
-PROJ_LIBRARIES = -L$(BLD_LIBDIR) -ljournal -lmcni -lmccomposite -lmcstas_compact -lmccomponents -lfparser
+PROJ_LIBRARIES = -L$(BLD_LIBDIR) -ljournal -lmccomposite -lmcni -lmcstas_compact
 
-# directory structure
-
-BUILD_DIRS = \
-	geometry \
-
-OTHER_DIRS = \
-
-RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
 
 #--------------------------------------------------------------------------
 #
 
 all: $(PROJ_TESTS)
-	BLD_ACTION="all" $(MM) recurse
 
 test:
 	for test in $(PROJ_TESTS) ; do $${test}; done
@@ -52,15 +39,8 @@ update: clean
 #--------------------------------------------------------------------------
 #
 
-test_random: test_random.cc
-	$(CXX) $(CXXFLAGS) $(LCXXFLAGS) -o $@ test_random.cc $(PROJ_LIBRARIES)
-
-test_rootfinding: test_rootfinding.cc
-	$(CXX) $(CXXFLAGS) $(LCXXFLAGS) -o $@ test_rootfinding.cc $(PROJ_LIBRARIES)
-
-test_fparser: test_fparser.cc
-	$(CXX) $(CXXFLAGS) $(LCXXFLAGS) -o $@ test_fparser.cc $(PROJ_LIBRARIES)
-
+test_choose_direction: test_choose_direction.cc 
+	$(CXX) $(CXXFLAGS) $(LCXXFLAGS) -o $@ test_choose_direction.cc $(PROJ_LIBRARIES)
 
 # version
 # $Id$
---
+++ packages/mccomponents/tests/libmccomponents/math/geometry/test_choose_direction.cc
@@ -0,0 +1,45 @@
+// -*- C++ -*-
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//                                   Jiao Lin
+//                      California Institute of Technology
+//                         (C) 2005 All Rights Reserved  
+//
+// {LicenseText}
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+
+
+#include <cassert>
+#include <iostream>
+#include "mcni/test/assert.h"
+#include "mccomponents/math/random/geometry.h"
+
+#include "journal/debug.h"
+
+using namespace std;
+using namespace mccomponents::math;
+char * jrnltag ="test_choose_direction";
+
+
+void test1()
+{
+
+}
+int main()
+{
+#ifdef DEBUG
+  //  journal::debug_t("mccomposite.geometry.ArrowIntersector").activate();
+//   journal::debug_t("mccomposite.geometry.Locator").activate();
+//   journal::debug_t("mccomposite.geometry.intersect").activate();
+//   journal::debug_t(jrnltag).activate();
+#endif
+  test1();
+}
+
+// version
+// $Id$
+
+// End of file 
