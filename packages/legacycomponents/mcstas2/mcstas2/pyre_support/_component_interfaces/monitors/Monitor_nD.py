ac904deb79ee3dc76ca02d74fb2c7d462b853679
Alexander Dementsov <dexity@gmail.com>
2010-11-20 02:55:45 +0000

    Partially implemented histogram for Monitor_nD
    git-svn-id: svn://danse.us/MCViNE/trunk@724 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/Monitor_nD.py
+++ packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/Monitor_nD.py
@@ -26,22 +26,23 @@ def get_histogram( monitor ):
 #    nx = core.nx; ny =core.ny
 #    n = nx * ny
 #    shape = nx, ny
-#
-#    xmin = core.xmin; xmax = core.xmax
-#    ymin = core.ymin; ymax = core.ymax
-#    dx = (xmax - xmin)/nx
-#    dy = (ymax - ymin)/ny
-#
-#    Iarr = bpptr2npyarr( core.getPSD_p_00( ), 'double', n ).copy()
-#    E2arr = bpptr2npyarr( core.getPSD_p2_00( ), 'double', n ).copy()
-#    Iarr.shape = E2arr.shape = shape
-#
-#    from histogram import histogram, axis, arange
-#    xaxis = axis( 'x', arange( xmin, xmax, dx ) )
-#    yaxis = axis( 'y', arange( ymin, ymax, dy ) )
-#
-#    h = histogram( 'I(x,y)', [xaxis,yaxis], data = Iarr, errors = E2arr )
-#    return h
+
+    xmin = core.xmin; xmax = core.xmax
+    ymin = core.ymin; ymax = core.ymax
+    dx = xmax - xmin
+    dy = ymax - ymin
+
+    # OUTPUT PARAMETERS (DEFS, Vars)
+    Iarr = bpptr2npyarr( core.getDEFS( ), 'double', n ).copy()  #?
+    #E2arr = bpptr2npyarr( core.getPSD_p2_00( ), 'double', n ).copy()
+    Iarr.shape = E2arr.shape = shape
+
+    from histogram import histogram, axis, arange
+    xaxis = axis( 'x', arange( xmin, xmax, dx ) )
+    yaxis = axis( 'y', arange( ymin, ymax, dy ) )
+
+    h = histogram( 'I(x,y)', [xaxis,yaxis], data = Iarr)    #, errors = E2arr )
+    return h
 
 __date__ = "$Nov 19, 2010 6:24:56 PM$"
 
