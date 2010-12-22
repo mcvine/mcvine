0bed4ae8013d8ab60d48e23086b505420ac393a6
Jiao Lin <linjiao@caltech.edu>
2010-12-22 07:30:47 +0000

    legacycomponents/mcstas2: added script "mcvine-compile-all-mcstas-components"
    git-svn-id: svn://danse.us/MCViNE/trunk@757 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/legacycomponents/mcstas2/bin/Make.mm
@@ -0,0 +1,60 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2004  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+PROJECT = mcstas2
+PACKAGE = bin
+
+# directory structure
+
+BUILD_DIRS = \
+
+OTHER_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
+
+#--------------------------------------------------------------------------
+#
+
+all: export
+	BLD_ACTION="all" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+
+
+EXPORT_PYTHON_MODULES = \
+
+
+EXPORT_BINS = \
+	mcvine-compile-all-mcstas-components \
+
+
+export:: export-binaries release-binaries
+
+
+include doxygen/default.def
+docs: export-doxygen-docs
+
+
+
+# version
+# $Id$
+
+# End of file
