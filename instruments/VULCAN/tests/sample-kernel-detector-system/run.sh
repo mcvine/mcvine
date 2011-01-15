de318270ee3871d9889673d5e78df98f3abae944
Alexander Dementsov <dexity@gmail.com>
2011-01-15 00:52:04 +0000

    Created test for sample kernel
    git-svn-id: svn://danse.us/MCViNE/trunk@814 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ instruments/VULCAN/tests/sample-kernel-detector-system/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./ssd -sample.xml=Al_SimplePowderDiffractionKernel/sampleassembly.xml -ncount=10000 -buffer_size=1000
---
+++ instruments/VULCAN/tests/vanadium-detector-system/run.sh
@@ -0,0 +1,22 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2010  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+# Running test simulation and drawing plots
+
+python vd
+python parseoutput.py
+gnuplot plot.gnu
+
+__date__ = "$Jan 14, 2011 4:19:33 PM$"
+
+
