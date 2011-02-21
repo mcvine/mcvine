698a48fb92a98fe4d7757cbaf077ff0180cfa6a9
Alexander Dementsov <dexity@gmail.com>
2011-02-21 07:41:59 +0000

    Updated expression conversion for wavelength
    git-svn-id: svn://danse.us/MCViNE/trunk@911 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mcni/mcni/pyre_components/ndmonitor.py
+++ packages/mcni/mcni/pyre_components/ndmonitor.py
@@ -26,7 +26,7 @@ neqs = [
 #   mcni.utils.conversion as conversion
 q2e = {
     'energy':       'conversion.VS2E * (vx*vx + vy*vy + vz*vz)',
-    'w':            'conversion.RV2W * 1/sqrt(vx*vx + vy*vy + vz*vz)',  # wavelength
+    'w':            'conversion.v2w(vx*vx + vy*vy + vz*vz)',  # wavelength
     'divx': 'vx/vz',
     'divy': 'vy/vz',
     'tof': 't',
