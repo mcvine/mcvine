f33318febb4626ab9bad554e96ebc90a1710b337
Jiao Lin <linjiao@caltech.edu>
2011-02-28 03:57:02 +0000

    mcvine/tests: started integration/tutorials for testing the examples in tutorials
    git-svn-id: svn://danse.us/MCViNE/trunk@953 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mcvine/tests/integration/tutorials/basics/CreateSimApp_TestCase.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2007 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+interactive = False
+
+simapp = 'test'
+outdir = 'out'
+
+import os, glob, shutil
+def cleanup():
+    pmls = glob.glob('test.pml*')
+    map(os.remove, pmls)
+    # clean up
+    if os.path.exists(outdir):
+        shutil.rmtree('out')
+
+
+def execute(cmd):
+    if os.system(cmd):
+        raise RuntimeError, "%r failed" %cmd
+
+
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        'mcvine instrument sim app: dumppml'
+        cleanup()
+
+        # 
+        cmd = 'mcvine-create-instrument-simulation-application -h'
+        execute(cmd)
+        
+        #
+        cmd = 'mcvine-create-instrument-simulation-application  --name=%s --components=source,monitor' % simapp
+        execute(cmd)
+
+        #
+        cmd = './%s -h' % simapp
+        execute(cmd)
+
+        #
+        cmd = './%s --source=MonochromaticSource --monitor=E_monitor' % simapp
+        execute(cmd)
+
+        #
+        cmd = './%s --source=MonochromaticSource --monitor=E_monitor --overwrite-datafiles --dump-pml' % simapp
+        execute(cmd)
+
+        #
+        cmd = './%s -h' % simapp
+        execute(cmd)
+
+        #
+        cmd = './%s --source.help-properties' % simapp
+        execute(cmd)
+
+        #
+        cmd = './%s --source.energy=60 --monitor.Emin=50 --monitor.Emax=70 --monitor.nchan=200 --dump-pml' % simapp
+        execute(cmd)
+        
+        #
+        cmd = './%s --ncount=1e4' % simapp
+        execute(cmd)
+
+        global interactive
+        if interactive:
+            cmd = 'PlotHist.py out/IE.h5'
+            execute(cmd)
+        
+        return
+
+    
+    pass  # end of TestCase
+
+
+
+def pysuite():
+    suite1 = unittest.makeSuite(TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+def main():
+    #debug.activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    unittest.TextTestRunner(verbosity=2).run(alltests)
+    
+    
+if __name__ == "__main__":
+    global interactive
+    interactive = True
+    main()
+    
+# version
+__id__ = "$Id$"
+
+# End of file 
