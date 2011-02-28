ba7a56aed89cf8fe47f2558b76e0afb825744384
Jiao Lin <linjiao@caltech.edu>
2011-02-28 02:23:18 +0000

    mcvine: another test case for --dump-pml
    git-svn-id: svn://danse.us/MCViNE/trunk@950 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mcvine/tests/integration/dumppml/dumppml_TestCase.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2007 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+outdir = 'out'
+pml = 'ssd.pml'
+
+import os, glob, shutil
+def cleanup():
+    # clean up
+    map(os.remove, glob.glob('ssd.pml*'))
+    if os.path.exists(outdir):
+        shutil.rmtree('out')
+
+
+def execute(cmd):
+    if os.system(cmd):
+        raise RuntimeError, "%r failed" %cmd
+
+
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        'mcvine instrument sim app: dumppml'
+        cleanup()
+
+        # do dump pml first time
+        cmd = './ssd -dump-pml'
+        execute(cmd)
+        # make sure output directory is not created
+        self.assert_(not os.path.exists(outdir))
+        # make sure ssd.pml is created
+        self.assert_(os.path.exists(pml))
+
+        # do dump pml second time
+        cmd = './ssd -dump-pml'
+        execute(cmd)
+        # make sure output directory is not created
+        self.assert_(not os.path.exists(outdir))
+        # make sure ssd.pml-<time> is created
+        timeformat = '%m-%d-%Y--'
+        import time
+        timestr = time.strftime(timeformat)
+        pattern = '%s.saved-%s*' % (pml, timestr)
+        self.assert_(glob.glob(pattern))
+
+        # run simulation
+        cmd = './ssd'
+        execute(cmd)
+        # make sure output directory is created
+        self.assert_(os.path.exists(outdir))
+
+        return
+
+    
+    pass  # end of TestCase
+
+
+
+def pysuite():
+    suite1 = unittest.makeSuite(TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+def main():
+    #debug.activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    unittest.TextTestRunner(verbosity=2).run(alltests)
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id$"
+
+# End of file 
