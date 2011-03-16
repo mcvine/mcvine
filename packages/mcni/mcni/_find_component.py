a91cc3c015446c2814e71976a74ba7b035e0a89b
Jiao Lin <linjiao@caltech.edu>
2011-03-16 22:29:41 +0000

    mcni/_find_component.py: try to find close matches of user request and give  user a hint
    git-svn-id: svn://danse.us/MCViNE/trunk@1036 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mcni/mcni/_find_component.py
+++ packages/mcni/mcni/_find_component.py
@@ -25,6 +25,7 @@ def find(type, category=None, supplier=None):
         suppliernames = [supplier]
     
     found = []; categoryin = category
+    close_matches = []
     for suppliername in suppliernames:
         supplier = suppliers[suppliername]
         if categoryin is None:
@@ -35,9 +36,21 @@ def find(type, category=None, supplier=None):
             types = supplier.listcomponentsincategory(category)
             if type in types:
                 found.append((category, suppliername))
+                continue
+            matches = get_close_matches(type, types)
+            if matches:
+                close_matches += matches
             continue
         continue
-    if not found: return
+    if not found: 
+        if close_matches:
+            import warnings
+            marker = '*'*70
+            msg = ("\n%s\nCannot find the component you requested: %r, \n"
+                   "maybe you mean %r?\n%s\n") %(
+                marker, type, close_matches[0], marker)
+            warnings.warn(msg)
+        return
 
     if len(found) > 1:
         raise RuntimeError, 'found more than 1 component for type %s: %s' % (
@@ -46,6 +59,7 @@ def find(type, category=None, supplier=None):
     category, suppliername = found[0]
     return type, category, suppliername
         
+from difflib import get_close_matches
 
 # version
 __id__ = "$Id$"
