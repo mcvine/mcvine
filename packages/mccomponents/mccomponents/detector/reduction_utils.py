8ad64d8ed7a38b87ca4b7147fa09e98beeef4ef1
Jiao Lin <linjiao@caltech.edu>
2011-03-28 03:59:49 +0000

    mccomponents/detector/reduction_utils.py: tentatively adding method events2IQE
    git-svn-id: svn://danse.us/MCViNE/trunk@1084 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/mccomponents/detector/reduction_utils.py
+++ packages/mccomponents/mccomponents/detector/reduction_utils.py
@@ -19,7 +19,19 @@ def events2Ipixtof( events, ipixtof ):
     return
 
 
-def events2IQE(events, pixelpositions, L1, vi, Ei=None):
+import pyre.units.length
+import pyre.units.energy
+import pyre.units.time
+
+def events2IQE(
+    eventsfile, nevents, 
+    pixelpositionsfile, npixels,
+    mod2sample=13.6*pyre.units.length.meter, Ei=100*pyre.units.energy.meV,
+    Qaxis=(0,10,0.1), Eaxis=(-95,95,1.),
+    tofUnit=100*pyre.units.time.ns,
+    toffset=0*pyre.units.time.s,
+    tofmax=0.015*pyre.units.time.s,
+    ):
     '''
     direct geometery inelastic neutron scattering reduction
     
@@ -34,13 +46,64 @@ def events2IQE(events, pixelpositions, L1, vi, Ei=None):
 
     vi = (0, 0, vi)
 
-    t1 = L1 / vi
+    t1 = mod2sample / vi
 
-    events: numpy array of events
-    pixelpositions: numpy array of pixel positions
+    eventsfile: events file
+    nevents: number of events
+    pixelpositionsfile:  unit:meter
+    npixels:
+    mod2sample: moderator to sample distance 
+    Ei: incident energy
+    Qaxis, Eaxis: (min, max, step) of Q and E
     '''
+    if not isinstance(eventsfile, basestring):
+        raise ValueError, "%s is not a str" % eventsfile
+    import os
+    if not os.path.exists(eventsfile):
+        raise IOError, "%s does not exist" % eventsfile
     
+    try:
+        nevents  = int(nevents)
+    except:
+        raise ValueError, "Cannot convert %s to integer" % nevents
+    
+    if not isinstance(pixelpositionsfile, basestring):
+        raise ValueError, "%s is not a str" % pixelpositionsfile
+    import os
+    if not os.path.exists(pixelpositionsfile):
+        raise IOError, "%s does not exist" % pixelpositionsfile
+    
+    try:
+        npixels  = int(npixels)
+    except:
+        raise ValueError, "Cannot convert %s to integer" % npixels
 
+    # for details of units used
+    # see Event2QE.h
+    mod2sample = mod2sample/pyre.units.length.meter
+    Ei = Ei/pyre.units.energy.meV
+    Qmin, Qmax, dQ = Qaxis
+    Emin, Emax, dE = Eaxis
+    tofUnit = tofUnit/pyre.units.time.second
+    toffset = tofUnit/pyre.units.time.microsecond
+    tofmax = tofmax/pyre.units.time.microsecond
+    args = [
+        eventsfile,
+        nevents,
+        Qmin, Qmax, dQ,
+        Ebegin, Eend, dE,
+        Ei, 
+        pixelpositionsfile,
+        npixels,
+        tofUnit,
+        mod2sample,
+        toffset,
+        tofmax
+        ]
+    cmd = 'events2iqe ' + ' '.join([str(a) for a in args])
+    print cmd
+    if os.system(cmd):
+        raise RuntimeError, "%s failed" % cmd
     return
 
 
