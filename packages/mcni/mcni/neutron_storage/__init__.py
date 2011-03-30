bbd1561ae4c13bb6ad762ec2ce381563c26f83f2
Jiao Lin <linjiao@caltech.edu>
2011-03-30 14:17:00 +0000

    mcni/mcni/neutron_storage/__init__.py: what a blunder in r1103!
    git-svn-id: svn://danse.us/MCViNE/trunk@1104 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mcni/mcni/neutron_storage/__init__.py
+++ packages/mcni/mcni/neutron_storage/__init__.py
@@ -32,7 +32,7 @@ def normalize(neutronsfile, N):
     """normalize the neutrons in the given file by the factor N"""
     from idf_usenumpy import read, write
     neutrons = read(neutronsfile)
-    if neutrons is not None:
+    if neutrons is None:
         return
 
     neutrons[:,-1]/=N
