c171d2510e403b3869a7cbf1b688ff26860e51a0
Jiao Lin <linjiao@caltech.edu>
2011-04-28 08:11:52 +0000

    mcni: test cases for testing neutron and neutron buffer
    git-svn-id: svn://danse.us/MCViNE/trunk@1148 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mcni/mcni/bindings/boostpython/_patch_neutronevents_bp_interface.py
+++ packages/mcni/mcni/bindings/boostpython/_patch_neutronevents_bp_interface.py
@@ -13,14 +13,30 @@ vector_Event.__str__ = __str__
     
 
 
-def NEB_appendNeutrons(self, neutrons, startindex, endindex):
+from mcni.mcnibp import NeutronEventBuffer
+def NEB_appendNeutrons(self, neutrons, startindex=None, endindex=None):
     """append neutrons to the end of this neutron buffer
 
     neutrons: the neutron buffer from which the new neutrons are to be obtained
             and appended to this buffer
     startindex, endindex: define the region from which neutrons are obtained
     """
+    if startindex is None:
+        startindex = 0
+    if endindex is None:
+        endindex =  len(neutrons)
     self.append(neutrons, startindex, endindex)
     return
-from mcni.mcnibp import NeutronEventBuffer
 NeutronEventBuffer.appendNeutrons = NEB_appendNeutrons
+NEB_snapshot_o = NeutronEventBuffer.snapshot
+def NEB_snapshot(self, n=None):
+    """take a snapshot of this neutron buffer, remove the invalid
+    ones (prob<0), and return a new neutron buffer.
+    
+    n: the number of neutrons in this buffer from which
+        the snapshot will be take. None means all neutrons
+    """
+    if n is None:
+        n = len(self)
+    return NEB_snapshot_o(self, n)
+NeutronEventBuffer.snapshot = NEB_snapshot
---
+++ packages/mcni/tests/mcni/neutron_TestCase.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2011 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+"""
+Test neutron and neutron buffer interfaces
+"""
+
+
+import unittestX as unittest
+import journal
+
+debug = journal.debug( "neutron_TestCase" )
+warning = journal.warning( "neutron_TestCase" )
+
+
+import mcni
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        "neutron: import"
+        from mcni import neutron
+        return
+    
+        
+    def test2(self):
+        "neutron"
+        from mcni import neutron
+        n = neutron(r=(0,0,0), v=(0,0,3000))
+        n = neutron(r=(0,0,0), v=(0,0,3000), time=1000.)
+        n = neutron(r=(0,0,0), v=(0,0,3000), time=1000., prob=10.)
+        n = neutron(r=(0,0,0), v=(0,0,3000), s=(0,1), time=1000., prob=10.)
+        return
+
+
+    def test3(self):
+        "neutron buffer: import"
+        from mcni import neutron_buffer
+        return
+    
+        
+    def test4(self):
+        "neutron buffer: swap"
+        from mcni import neutron_buffer
+        b1 = neutron_buffer(1)
+        b2 = neutron_buffer(2)
+        b1.swap(b2)
+        self.assertEqual(len(b1), 2)
+        self.assertEqual(len(b2), 1)
+        return
+    
+        
+    def test5(self):
+        "neutron buffer: appendNeutrons"
+        from mcni import neutron_buffer
+        b1 = neutron_buffer(1)
+        b2 = neutron_buffer(2)
+        
+        b1.appendNeutrons(b2, 0, len(b2))
+        self.assertEqual(len(b1), 3)
+        
+        b1.appendNeutrons(b2)
+        self.assertEqual(len(b1), 5)
+        return
+    
+        
+    def test6(self):
+        "neutron buffer: get element"
+        from mcni import neutron_buffer
+        b1 = neutron_buffer(1)
+        n = b1[0]
+        # print n
+        return
+
+
+    def test7(self):
+        "neutron buffer: set element"
+        from mcni import neutron_buffer, neutron
+        b1 = neutron_buffer(1)
+        n = neutron(r=(0,10,0))
+        b1[0] = n
+        self.assertEqual(b1[0].state.position[1], 10)
+        return
+
+
+    def test8(self):
+        "neutron buffer: clear"
+        from mcni import neutron_buffer, neutron
+        
+        b1 = neutron_buffer(10)
+        self.assertEqual(len(b1), 10)
+
+        b1.clear()
+        self.assertEqual(len(b1), 0)
+        return
+
+
+    def test9(self):
+        "neutron buffer: snapshot"
+        from mcni import neutron_buffer, neutron
+        
+        b1 = neutron_buffer(10)
+        self.assertEqual(len(b1), 10)
+        b1[5] = neutron(prob=-1)
+
+        b2 = b1.snapshot()
+        self.assertEqual(len(b2), 9)
+
+        b2 = b1.snapshot(9)
+        self.assertEqual(len(b2), 8)
+
+        b2 = b1.snapshot(4)
+        self.assertEqual(len(b2), 4)
+        return
+
+
+    pass  # end of TestCase
+
+
+    
+def pysuite():
+    suite1 = unittest.makeSuite(TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+def main():
+    #debug.activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    res = unittest.TextTestRunner(verbosity=2).run(alltests)
+    import sys; sys.exit(not res.wasSuccessful())
+
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id: mcni_TestCase.py 1126 2011-04-10 03:05:40Z linjiao $"
+
+# End of file 
