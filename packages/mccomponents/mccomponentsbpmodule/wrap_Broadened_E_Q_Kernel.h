260514e9dfa1dabb45466f8f10149930985b5777
Jiao Lin <linjiao@caltech.edu>
2011-05-26 00:15:27 +0000

    mccomponentsbpmodule/wrap_Broadened_E_Q_Kernel.h: another one
    git-svn-id: svn://danse.us/MCViNE/trunk@1160 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mccomponents/mccomponentsbpmodule/wrap_Broadened_E_Q_Kernel.h
@@ -0,0 +1,48 @@
+// -*- C++ -*-
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//                                   Jiao Lin
+//                      California Institute of Technology
+//                         (C) 2007 All Rights Reserved
+//
+// {LicenseText}
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+
+
+#include <sstream>
+#include <boost/python.hpp>
+#include "mccomponents/kernels/sample/Broadened_E_Q_Kernel.h"
+#include "mccomponents/math/Fx_fromExpr.h"
+
+
+namespace wrap_mccomponents {
+
+  struct Wrap_Broadened_E_Q_Kernel{
+
+    typedef mccomponents::math::Fx_fromExpr fx_t;
+    typedef mccomponents::kernels::Broadened_E_Q_Kernel<fx_t, fx_t, fx_t> \
+    kernel_t;
+
+    static kernel_t * newKernel
+    (const std::string & E_Q,
+     const std::string & S_Q,
+     const std::string & sigma_Q,
+     double Qmin, double Qmax,
+     double absorption_cross_section, 
+     double scattering_cross_section
+     );
+
+    Wrap_Broadened_E_Q_Kernel();
+
+  };
+
+}
+
+
+// version
+// $Id: wrap_ConstantQEKernel.cc 601 2010-10-03 19:55:29Z linjiao $
+
+// End of file 
