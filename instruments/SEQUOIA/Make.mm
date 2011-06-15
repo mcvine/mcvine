0b88b73e88444078c11e0a1855d5af322080fd28
Jiao Lin <linjiao@caltech.edu>
2011-06-15 18:01:54 +0000

    sequoia: added Make.mm files
    git-svn-id: svn://danse.us/MCViNE/trunk@1203 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ instruments/SEQUOIA/Make.mm
@@ -0,0 +1,53 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2004  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+PROJECT = mcvine/SEQUOIA
+
+
+PROJ_TIDY += *.log *.tmp
+
+# directory structure
+
+BUILD_DIRS = \
+	SEQUOIA \
+	applications \
+	etc \
+	resources \
+	simulations \
+
+OTHER_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
+
+#--------------------------------------------------------------------------
+#
+
+all:
+	BLD_ACTION="all" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+docs::
+	BLD_ACTION="docs" $(MM) recurse
+
+# version
+# $Id: Make.mm 1112 2011-03-31 06:12:19Z linjiao $
+
+# End of file
---
+++ instruments/SEQUOIA/etc/Make.mm
@@ -0,0 +1,43 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2005  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+PROJECT = mcvine/instruments/SEQUOIA
+
+BUILD_DIRS = \
+	sequoia_moderator2sample \
+
+OTHER_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
+
+#--------------------------------------------------------------------------
+#
+
+all: export
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+export::
+	BLD_ACTION="export" $(MM) recurse
+
+
+# version
+# $Id: Make.mm 776 2011-01-07 05:34:20Z linjiao $
+
+# End of file
