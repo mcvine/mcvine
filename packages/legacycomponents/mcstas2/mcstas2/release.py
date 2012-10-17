9c0b3b3ae6f3c25003c7f55900f48410fa6c2358
Jiao Lin <linjiao@caltech.edu>
2012-10-17 23:03:24 +0000

    mcstas2/release.py: refactored -- some code moved to mcvine.deployment_info
    git-svn-id: svn://danse.us/MCViNE/trunk@1233 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/legacycomponents/mcstas2/mcstas2/release.py
+++ packages/legacycomponents/mcstas2/mcstas2/release.py
@@ -13,19 +13,8 @@
 
 
 import os
-dvdir = os.environ.get('DV_DIR')
-exportroot = os.environ.get('EXPORT_ROOT')
-pyredir = os.environ.get("PYRE_DIR")
-mcvinedir = os.environ.get("MCVINE_DIR")
 
-
-if dvdir and exportroot: type = 'developer'
-else: 
-    type = 'user'
-    mcvinedir = mcvinedir or pyredir or exportroot
-    if not mcvinedir:
-        raise RuntimeError, "environment variable MCVINE_DIR was not defined. please define it to the path of the mcvine installation"
-
+from mcvine.development_info import mcvinedir
 
 component_library_dir = os.environ.get('MCSTAS_COMPONENT_LIBDIR', None)
 if component_library_dir is None:
