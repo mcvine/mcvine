c2b4a522ee47eb98d9de2ffb8f0a6756c2bc91f4
Jiao Lin <linjiao@caltech.edu>
2012-10-17 23:02:26 +0000

    mcvine/mcvine/deployment_info.py: moved code for testing deployment environment from mcstas2.release to here.
    git-svn-id: svn://danse.us/MCViNE/trunk@1232 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mcvine/mcvine/deployment_info.py
+++ packages/mcvine/mcvine/deployment_info.py
@@ -16,6 +16,20 @@ import os
 debug = os.environ.get('DEBUG', False)
 
 
+# paths
+dvdir = os.environ.get('DV_DIR')
+exportroot = os.environ.get('EXPORT_ROOT')
+pyredir = os.environ.get("PYRE_DIR")
+mcvinedir = os.environ.get("MCVINE_DIR")
+
+if dvdir and exportroot: type = 'developer'
+else: 
+    type = 'user'
+    mcvinedir = mcvinedir or pyredir or exportroot
+    if not mcvinedir:
+        raise RuntimeError, "environment variable MCVINE_DIR was not defined. please define it to the path of the mcvine installation"
+
+
 # version
 __id__ = "$Id: __init__.py 601 2010-10-03 19:55:29Z linjiao $"
 
