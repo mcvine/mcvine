53f5ebf6673b63b6c59ce85c58dcc2d98cc56c8a
Jiao Lin <linjiao@caltech.edu>
2012-11-21 17:40:58 +0000

    added components/EventAreaMonitor_TestCase.py
    git-svn-id: svn://danse.us/MCViNE/trunk@1261 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mcni/tests/mcni/components/EventAreaMonitor_TestCase.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2007-2010 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+interactive = False
+
+
+import unittestX as unittest
+
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        'NDMonitor'
+        
+        from mcni.components.EventAreaMonitor import EventAreaMonitor
+        m = EventAreaMonitor('eam')
+        
+        N = 10
+        from mcni import neutron_buffer, neutron
+        b = neutron_buffer(N)
+        for i in range(N):
+            b[i] = neutron()
+            continue
+
+        m.process(b)
+
+        self.assertEqual(len(m.events), N)
+        self.assert_((m.events == m.events[0]).all())
+        e = m.events[0]
+        self.assertEqual(e['pixelID'], 5050)
+        self.assertEqual(e['tofChannelNo'], 0)
+        self.assertEqual(e['p'], 1.)
+        return
+        
+
+    pass # end of TestCase
+
+
+def pysuite():
+    suite1 = unittest.makeSuite(TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+
+def main():
+    #debug.activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    res = unittest.TextTestRunner(verbosity=2).run(alltests)
+    import sys; sys.exit(not res.wasSuccessful())
+
+    return
+
+
+if __name__ == "__main__": 
+    interactive = True
+    main()
+
+    
+# version
+__id__ = "$Id: NDMonitor_TestCase.py 1144 2011-04-28 04:02:13Z linjiao $"
+
+# End of file 
