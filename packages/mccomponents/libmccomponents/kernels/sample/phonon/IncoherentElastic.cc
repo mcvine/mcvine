b77a162fba27e48c07c00720b588eff8346ceff8
Jiao Lin <linjiao@caltech.edu>
2012-11-27 20:34:51 +0000

    libmccomponents/kernels/sample/phonon/IncoherentElastic.cc: fixed a bug in determining final velocity
    git-svn-id: svn://danse.us/MCViNE/trunk@1291 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/libmccomponents/kernels/sample/phonon/IncoherentElastic.cc
+++ packages/mccomponents/libmccomponents/kernels/sample/phonon/IncoherentElastic.cc
@@ -18,6 +18,9 @@
 #include "mccomponents/math/random.h"
 
 
+// #define DEBUG
+
+
 #ifdef DEEPDEBUG
 #define __DEBUG__PHNN__INCOHELAST__
 #endif
@@ -45,14 +48,19 @@ struct mccomponents::kernels::phonon::IncoherentElastic::Details {
   Details() : debug( jrnltag ) {}
 #endif
 
+  const static double m_epsilon;
 };
 
 
 #ifdef __DEBUG__PHNN__INCOHELAST__
-const char * mccomponents::kernels::phonon::IncoherentElastic::
-Details::jrnltag 
+const char mccomponents::kernels::phonon::IncoherentElastic::
+Details::jrnltag []
 = "phonon_incoherent_elastic_kernel";
 #endif
+
+const double mccomponents::kernels::phonon::IncoherentElastic::
+Details::m_epsilon
+= 1e-4;
 
 
 mccomponents::kernels::phonon::IncoherentElastic::
@@ -95,6 +103,7 @@ mccomponents::kernels::phonon::IncoherentElastic::scattering_coefficient
 ( const neutron_t & ev )
 {
   float_t ret = m_total_scattering_xs/m_uc_vol;
+  // std::cout << "scattering_coefficient: " << ret << std::endl;
   // convert to m**-1
   return ret * 1.e2;
 }
@@ -127,21 +136,36 @@ mccomponents::kernels::phonon::IncoherentElastic::scatter
   m_details->debug
     << "theta: " << theta << ", "
     << "phi: " << phi << ", "
+    << "Q: " << Q << ", "
+    << "dw core: " << m_dw_core << ", "
+    << "dw: " << dw << ", "
     << journal::endl;
 #endif
 
   // scattered neutron velocity vector
-  double vx = vi*sin(theta)*cos(phi);
-  double vy = vi*sin(theta)*sin(phi);
-  double vz = vi*cos(theta);
+  // == coordinate system ==
+  V_t e1 = state.velocity; e1.normalize();
+  // if e1 is not in z-direction
+  // we set e2 to be the cross product of e1 and (0,0,1)
+  // if e1 is right on the z-direction, that means e1 = (0,0,1)
+  // and we set e2 = (1,0,0) or whatever
+  K_t e2;
+  if (std::abs(e1.x)>m_details->m_epsilon || std::abs(e1.y)>m_details->m_epsilon) { 
+    e2 = V_t(0,0,1) * e1; e2.normalize();
+  } else {
+    e2 = V_t(1,0,0);
+  }
+  V_t e3 = e1 * e2;
+  // == v_f ==
+  V_t v_f = sin(theta)*cos(phi) * e2 
+    + sin(theta)*sin(phi) * e3
+    + cos(theta) * e1;
+  v_f = v_f *  vi; // elastic scattering
+  state.velocity = v_f;
 
   // adjust probability of neutron event
   // normalization factor is 2pi*pi/4pi = pi/2
   ev.probability *= sin(theta) * (mcni::PI/2) * dw;
-
-  typedef mcni::Vector3<double> V3d;
-  V3d vf(vx,vy,vz);
-  state.velocity = vf;
 
 #ifdef DEBUG
   m_details->debug
