0ea0e3a9f3d47e7f51954a50dcf5e1f4b194fcfb
Jiao Lin <linjiao@caltech.edu>
2012-12-19 00:52:29 +0000

    added test for mccomponents.sample.diffraction.parsers.laz
    git-svn-id: svn://danse.us/MCViNE/trunk@1397 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mccomponents/tests/mccomponents/sample/diffraction/parsers_TestCase.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2007 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+
+import unittestX as unittest
+import journal
+
+#debug = journal.debug( "TestCase" )
+#warning = journal.warning( "TestCase" )
+
+
+import mcni
+
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        laz = 'Al.laz'
+        text = open(laz).read()
+        from mccomponents.sample.diffraction.parsers.laz import parse
+        peaks = parse(text)
+        print(peaks)
+        return
+        
+        
+    pass  # end of TestCase
+
+
+def pysuite():
+    suite1 = unittest.makeSuite(TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+
+def main():
+    #debug.activate()
+    #journal.debug('phonon_coherent_inelastic_polyxtal_kernel').activate()
+    #journal.debug('random').activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    res = unittest.TextTestRunner(verbosity=2).run(alltests)
+    import sys; sys.exit(not res.wasSuccessful())
+
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id: dispersion_TestCase.py 1126 2011-04-10 03:05:40Z linjiao $"
+
+# End of file 
