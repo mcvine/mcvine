91733be6efdd1a0fc3c98d27d6507f00a2c4c4d6
Jiao Lin <linjiao@caltech.edu>
2013-01-20 03:00:47 +0000

    mcstas2/.../pyre_support/_component_interfaces/monitors/IQE_monitor.py: improved to have better statistics
    git-svn-id: svn://danse.us/MCViNE/trunk@1432 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/IQE_monitor.py
+++ packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/IQE_monitor.py
@@ -35,7 +35,7 @@ class ComponentInterface(base):
         context = self.simulation_context
         # the master node need to compute the norm
         if context.mpiRank == 0:
-            norm = getNormalization(self, N=1000000)
+            norm = getNormalization(self)
         # if there are no other nodes, we are fine
         if not context.mpiSize:
             return norm
@@ -73,33 +73,47 @@ def get_histogram( monitor ):
     return h
 
 
-def getNormalization(monitor, N=1000000, epsilon=1e-7):
+def getNormalization(monitor, N=None, epsilon=1e-7):
     # randomly shoot neutrons to monitor in 4pi solid angle
-    
+    print "* start computing normalizer..."
     core = monitor.core()
+    if N is None:
+        N = core.nQ * core.nE * 10000
+    
     import mcni, random, mcni.utils.conversion as conversion, math, os
+    import numpy as np
+
+    # incident velocity
+    vi = conversion.e2v(core.Ei)
     
     # 1. create neutrons
-    neutrons = mcni.neutron_buffer(N)
-    vi = conversion.e2v(core.Ei)
-    for i in range(N):
+    def make_neutrons(N):
+        neutrons = mcni.neutron_buffer(N)
+        #
         # randomly select E, the energy transfer
-        E = core.Emin + random.random() * (core.Emax-core.Emin)
+        E = core.Emin + np.random.random(N) * (core.Emax-core.Emin)
         # the final energy
         Ef = core.Ei - E
         # the final velocity
         vf = conversion.e2v(Ef)
-        # choose theta
-        theta = random.random() * math.pi
-        # choose phi
-        phi = random.random() * math.pi * 2.
+        # choose cos(theta) between -1 and 1
+        cos_t = np.random.random(N) * 2 - 1
+        # theta
+        theta = np.arccos(cos_t)
+        # sin(theta)
+        sin_t = np.sin(theta)
+        # phi: 0 - 2pi
+        phi = np.random.random(N) * 2 * np.pi
         # compute final velocity vector
-        vfv = vf*math.sin(theta)*math.cos(phi), vf*math.sin(theta)*math.sin(phi), vf*math.cos(theta)
-        # prob
-        prob = math.sin(theta) * (vf/vi)
-        # the neutron
-        neutrons[i] = mcni.neutron(prob=prob, v=vfv)
-        continue
+        vx,vy,vz = vf*sin_t*np.cos(phi), vf*sin_t*np.sin(phi), vf*cos_t
+        # neutron position, spin, tof are set to zero
+        x = y = z = sx = sy = t = np.zeros(N, dtype="float64")
+        # probability
+        prob = np.ones(N, dtype="float64") * (vf/vi)
+        # XXX: this assumes a specific data layout of neutron struct
+        n_arr = np.array([x,y,z,vx,vy,vz, sx,sy, t, prob]).T.copy()
+        neutrons.from_npyarr(n_arr)
+        return neutrons
     
     # 2. create a copy of the original monitor
     from mcstas2 import componentfactory
@@ -120,12 +134,22 @@ def getNormalization(monitor, N=1000000, epsilon=1e-7):
     monitorcopy = cf('monitor', **kwds)
     
     # 3. send neutrons to monitor copy
-    monitorcopy.process(neutrons)
+    N1 = 0; dN = int(1e6)
+    print "  - total neutrons needed :", N
+    while N1 < N:
+        n = min(N-N1, dN)
+        neutrons = make_neutrons(n)
+        monitorcopy.process(neutrons)
+        N1 += n
+        print "  - processed %s" % N1
+        continue
     h = get_histogram(monitorcopy)
     # for debug
     # import histogram.hdf as hh
     # hh.dump(h, 'tmp.h5', '/', 'c')
     h.I[h.I<epsilon] = 1
+    #
+    print "  - done computing normalizer"
     return h
 
 
