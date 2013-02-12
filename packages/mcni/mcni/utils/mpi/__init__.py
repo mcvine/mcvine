7628db47909ff72a873b555d72ca6183f982aa0f
Jiao Lin <linjiao@caltech.edu>
2013-02-12 17:23:08 +0000

    mcni/utils/mpi/__init__.py: support env var "MCVINE_MPI_BINDING"
    git-svn-id: svn://danse.us/MCViNE/branches/try_mpi4py@1464 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mcni/mcni/utils/mpi/__init__.py
+++ packages/mcni/mcni/utils/mpi/__init__.py
@@ -15,22 +15,37 @@
 import journal
 info = journal.info( 'mcni.utils.mpi' )
 
+import os
+mpi_binding_choice = os.environ.get('MCVINE_MPI_BINDING')
+
 def _find_mpi_binding():
-    import use_mpi4py as mod
-    if mod.world:
-        return mod
-    import use_pyre as mod
-    if mod.world:
-        return mod
+    choices = ['mpi4py', 'pyre']
+    for c in choices:
+        mod = use_mpi_binding(c)
+        if mod: return mod
+        continue
     return
 
-b = _find_mpi_binding()
+
+def use_mpi_binding(name):
+    exec "from . import use_%s as mod" % name
+    if mod.world:
+        return mod
+
+if mpi_binding_choice:
+    b = use_mpi_binding(mpi_binding_choice)
+else:
+    b = _find_mpi_binding()
 if b:
     names = ['size', 'rank', 'world', 'send', 'receive']
     for name in names:
         exp = '%s = b.%s' % (name, name)
         exec exp
         continue
+else:
+    world = None
+    size = rank = 0
+    send = receive = None
 
 
 # version
