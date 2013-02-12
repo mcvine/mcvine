a0c9f6c0f463e9fdef07cfd286744436e7c2de37
Jiao Lin <linjiao@caltech.edu>
2013-02-12 04:25:27 +0000

    tests/mcni/pyre_support: added serial_app_TestCase.py and parallel_app_TestCase.py
    git-svn-id: svn://danse.us/MCViNE/branches/try_mpi4py@1462 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mcni/tests/mcni/pyre_support/serial_app_TestCase.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2007 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+
+import unittestX as unittest
+import journal
+
+debug = journal.debug( "mcni.pyre_support.test" )
+warning = journal.warning( "mcni.pyre_support.test" )
+
+
+
+class TestCase(unittest.TestCase):
+
+
+    def test(self):
+        'mcni.pyre_support: a simple pyre simulation app'
+        instrument = App('test-serial-app')
+        instrument.testFacility = self
+        instrument.run()
+        return
+    
+        
+    pass  # end of TestCase
+
+
+
+from mcni.pyre_support.MpiApplication import Application as base
+class App(base):
+
+    class Inventory( base.Inventory ):
+
+        import pyre.inventory
+        pass # end of Inventory
+
+
+    def main(self):
+        super(App, self).main()
+        print "in app.main()"
+        return
+
+
+
+def pysuite():
+    suite1 = unittest.makeSuite(TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+def main():
+    #debug.activate()
+    import journal
+    journal.info('mpirun').activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    res = unittest.TextTestRunner(verbosity=2).run(alltests)
+    import sys; sys.exit(not res.wasSuccessful())
+
+    return
+
+
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id$"
+
+# End of file 
