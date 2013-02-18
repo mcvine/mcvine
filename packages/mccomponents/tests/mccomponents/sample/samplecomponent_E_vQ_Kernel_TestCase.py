de742b5b5f93b37edf4eb12afca498bff86d179f
Jiao Lin <linjiao@caltech.edu>
2013-02-18 05:51:45 +0000

    tests/mccomponents/sample/samplecomponent_E_vQ_Kernel_TestCase.py: added
    git-svn-id: svn://danse.us/MCViNE/trunk@1487 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mccomponents/tests/mccomponents/sample/samplecomponent_E_vQ_Kernel_TestCase.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2005-2013  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+standalone = True
+
+import unittestX as unittest
+
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        'mccomponents.sample.samplecomponent: E_vQ_Kernel'
+        import mcni
+        from mcni.utils import conversion
+        
+        ei = 60
+        vil = conversion.e2v(ei)
+        vi = (0,0,vil)
+        neutron = mcni.neutron( 
+            r = (0,0,0), v = vi, 
+            time = 0, prob = 1 )
+        
+        from mcni.components.MonochromaticSource import MonochromaticSource
+        component1 = MonochromaticSource('source', neutron)
+        
+        from mccomponents.sample import samplecomponent
+        component2 = samplecomponent( 'Al', 'sampleassemblies/Al-E_vQ-kernel/sampleassembly.xml' )
+        E_Q = '20 + 5 * sin(Qx+Qy+Qz)' # in sampleassemblies/Al-E_vQ-kernel/Al-scatterer.xml
+        
+        instrument = mcni.instrument( [component1, component2] )
+        
+        geometer = mcni.geometer()
+        geometer.register( component1, (0,0,0), (0,0,0) )
+        geometer.register( component2, (0,0,1), (0,0,0) )
+        
+        N0 = 1000
+        neutrons = mcni.neutron_buffer(N0)
+        
+        mcni.simulate( instrument, geometer, neutrons )
+        
+        N = len(neutrons)
+        
+        import numpy.linalg as nl
+        import numpy as np
+        from math import sin
+        for i in range(N):
+            neutron = neutrons[i]
+            vf = np.array(neutron.state.velocity)
+            diffv = vi - vf
+            Q = conversion.V2K * diffv
+            Qx, Qy, Qz = Q
+            ef = conversion.v2e(nl.norm(vf))
+            E = ei - ef
+            # print E, Q, neutron
+            E1 = eval(E_Q)
+            self.assertAlmostEqual(E, E1)
+            continue
+
+        return
+    
+
+    pass  # end of TestCase
+
+
+def main():
+    unittest.main()
+    return
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id: samplecomponent_ConstantEnergyTransferKernel_TestCase.py 696 2010-11-09 06:23:06Z linjiao $"
+
+# End of file 
