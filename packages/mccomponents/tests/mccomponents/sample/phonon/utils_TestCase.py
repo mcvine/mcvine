739bebf5b870947130577df45dab5cb8a199e5c4
Jiao Lin <linjiao@caltech.edu>
2013-03-18 05:57:36 +0000

    added tests/mccomponents/sample/phonon/utils_testCase.py
    git-svn-id: svn://danse.us/MCViNE/trunk@1510 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mccomponents/tests/mccomponents/sample/phonon/utils_TestCase.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2007 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+
+import unittestX as unittest
+import journal
+
+#debug = journal.debug( "TestCase" )
+#warning = journal.warning( "TestCase" )
+
+
+import mcni
+
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        from dos import loadDOS
+        dos = loadDOS()
+        E = dos.energy; g = dos.I
+        import pylab
+        pylab.plot(E, g)
+        pylab.show()
+        from mccomponents.sample.phonon.utils import fitparabolic
+        E, g = fitparabolic(E,g)
+        pylab.plot(E,g)
+        pylab.show()
+        return
+        
+        
+    pass  # end of TestCase
+
+
+def pysuite():
+    suite1 = unittest.makeSuite(TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+
+def main():
+    #debug.activate()
+    #journal.debug('phonon_coherent_inelastic_polyxtal_kernel').activate()
+    #journal.debug('random').activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    res = unittest.TextTestRunner(verbosity=2).run(alltests)
+    import sys; sys.exit(not res.wasSuccessful())
+
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id: dispersion_TestCase.py 1126 2011-04-10 03:05:40Z linjiao $"
+
+# End of file 
