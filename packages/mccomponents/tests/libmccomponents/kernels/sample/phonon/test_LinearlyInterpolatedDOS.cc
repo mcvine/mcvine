62787b466cb15f919276a27d1618e2efd783fa57
Jiao Lin <linjiao@caltech.edu>
2013-03-18 17:34:28 +0000

    tests/libmccomponents/.../phonon/test_LinearlyInterpolatedDOS.cc: added a test for method "sod"
    git-svn-id: svn://danse.us/MCViNE/trunk@1520 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/tests/libmccomponents/kernels/sample/phonon/test_LinearlyInterpolatedDOS.cc
+++ packages/mccomponents/tests/libmccomponents/kernels/sample/phonon/test_LinearlyInterpolatedDOS.cc
@@ -37,12 +37,12 @@ namespace test {
   void test2()
   {
     
-    array_t Z(51);
-    for (size_t i=0; i<51; i++) Z[i] = i*i;
+    array_t Z(501);
+    for (size_t i=0; i<501; i++) Z[i] = i*i;
     
-    w_t dos(0., 1., 51, Z);
+    w_t dos(0., .1, 501, Z);
     
-    size_t n(200);
+    size_t n(500);
     double e0 = 0, e1 = 50, de = (e1-e0)/n;
     
     double integrated = 0.;
@@ -50,8 +50,12 @@ namespace test {
       integrated += dos.value( e0 + de*i );
     }
     integrated*=de;
-    
     assert ( std::abs(integrated-1.) < 0.05 );
+
+    // test method sod()
+    double sod = dos.sod(), sod_expected = 3./e1/e1/e1;
+    assert (std::abs(sod-sod_expected)/sod_expected < 1e-2);
+    // std::cout << dos.sod() << std::endl;
   }
   
 }
