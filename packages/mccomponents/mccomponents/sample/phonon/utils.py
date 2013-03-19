92357a4dd28ce79457371cd9e4e3a951ce4c1761
Jiao Lin <linjiao@caltech.edu>
2013-03-19 00:36:29 +0000

    bin/mcvine-debye-waller-core-from-phonon-dos: refactored slightly by move some code to new method mccomponents.sample.phonon.utils.nice_dos
    git-svn-id: svn://danse.us/MCViNE/trunk@1522 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/mccomponents/sample/phonon/utils.py
+++ packages/mccomponents/mccomponents/sample/phonon/utils.py
@@ -12,7 +12,29 @@
 #
 
 
+"""
+utils for Density of States
+"""
+
 import numpy as np
+
+
+def nice_dos(E, g):
+    # .. in crease number of points if necessary
+    if len(E) < 400:
+        dE = E[-1]/400.
+        E1 = np.arange(0, E[-1], dE)
+        g1 = np.interp(E1, E, g)
+        E, g = E1, g1
+    # .. fit parabolic
+    try:
+        E,g = fitparabolic(E,g)
+    except ParabolicFittingError:
+        g = smooth(g, window_len=21)
+        E,g = fitparabolic(E,g)
+    # normalize
+    g /= g.sum() * (E[1] - E[0])
+    return E,g
 
 
 class ParabolicFittingError(Exception): pass
@@ -45,6 +67,69 @@ def linear_regression(x,y):
     SS_err = ((y - c*x)**2).sum()
     R2 = 1-SS_err/SS_tot
     return c, R2
+
+
+# copied from
+# http://www.scipy.org/Cookbook/SignalSmooth
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+    import numpy
+    
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=numpy.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=numpy.ones(window_len,'d')
+    else:
+        w=eval('numpy.'+window+'(window_len)')
+
+    y=numpy.convolve(w/w.sum(),s,mode='valid')
+    return y[(window_len/2-1):-(window_len/2)-1]
+    return y
+
 
 
 # version
