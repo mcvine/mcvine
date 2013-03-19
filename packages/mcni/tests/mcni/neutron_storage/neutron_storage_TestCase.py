306f3111d0ddff9bc0017770e0c6c4889f4dcec7
Jiao Lin <linjiao@caltech.edu>
2013-03-19 20:58:35 +0000

    neutron_storage/neutron_storage_TestCase.py: added a sanity check test to insure the "npy" load/dump methods are compatible with "storage" class. add a test with large neutron buffer.
    git-svn-id: svn://danse.us/MCViNE/trunk@1533 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mcni/tests/mcni/neutron_storage/neutron_storage_TestCase.py
+++ packages/mcni/tests/mcni/neutron_storage/neutron_storage_TestCase.py
@@ -14,12 +14,12 @@
 
 
 import unittestX as unittest
-import journal
+import mcni.neutron_storage as mns, os
 
+
+import journal
 debug = journal.debug( "mcni.neutron_storage.test" )
 warning = journal.warning( "mcni.neutron_storage.test" )
-
-import mcni.neutron_storage as mns
 
 
 class TestCase(unittest.TestCase):
@@ -40,20 +40,77 @@ class TestCase(unittest.TestCase):
         return
         
 
+    def test1(self):
+        'neutron_storage: mix npy implementation and non-npy one'
+        import mcni
+        neutrons = mcni.neutron_buffer( 7 )
+        out = 'tmp-nst-test1.ns'
+        if os.path.exists(out): os.remove(out)
+        storage = mns.storage(out, mode='w')
+        storage.write(neutrons)
+        neutrons1 = mns.load( 'neutrons.dat' )
+        return
+        
+
     def test2(self):
         'normalize'
         import mcni
+        # create a dummy input
         neutrons = mcni.neutron_buffer( 10 )
         for n in neutrons:
             n.probability = 1
             continue
         out = 'tmp-nst-test2.ns'
         mns.dump(neutrons, out)
-        mns.normalize(out, 10.)
-        neutrons2 = mns.load(out)
+
+        # try normalize out-of-place
+        out2 = 'tmp-nst-test2-normalized.ns'
+        if os.path.exists(out2): os.remove(out2)
+        mns.normalize(out, 10., out2)
+        neutrons2 = mns.load(out2)
+        # and see if it is done correctly
         for n in neutrons2:
             self.assertAlmostEqual(n.probability, .1)
             continue
+
+        # try normalize in-place
+        mns.normalize(out, 10.)
+        neutrons2 = mns.load(out)
+        # and see if it is done correctly
+        for n in neutrons2:
+            self.assertAlmostEqual(n.probability, .1)
+            continue
+        return
+        
+
+    def test2a(self):
+        'normalize: large buffer'
+        import mcni
+        # create a dummy input
+        neutrons = mcni.neutron_buffer( int(3e6) )
+        narr = neutrons.to_npyarr()
+        narr[:, -1] = 1
+        neutrons.from_npyarr(narr)
+        out = 'tmp-nst-test2a.ns'
+        mns.dump(neutrons, out)
+        del neutrons
+        
+        # try normalize out-of-place
+        out2 = 'tmp-nst-test2a-normalized.ns'
+        if os.path.exists(out2): os.remove(out2)
+        mns.normalize(out, 10., out2)
+        neutrons2 = mns.load(out2)
+        # and see if it is done correctly
+        narr = neutrons2.to_npyarr()
+        self.assertTrue((narr[:, -1] == .1).all())
+        del neutrons2, narr
+        
+        # try normalize in-place
+        mns.normalize(out, 10.)
+        neutrons2 = mns.load(out)
+        # and see if it is done correctly
+        narr = neutrons2.to_npyarr()
+        self.assertTrue((narr[:, -1] == .1).all())
         return
         
     pass  # end of TestCase
@@ -76,7 +133,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
+    # main()
     
 # version
 __id__ = "$Id$"
