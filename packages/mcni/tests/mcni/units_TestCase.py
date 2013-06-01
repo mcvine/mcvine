cd87f93b5d5c918bd990e8adc3c7d4bcb70d0cdb
Jiao Lin <linjiao@caltech.edu>
2013-06-01 15:28:41 +0000

    tests/mcni/units_TestCase.py: added
    git-svn-id: svn://danse.us/MCViNE/trunk@1562 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mcni/tests/mcni/units_TestCase.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2007 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+
+import unittestX as unittest
+import journal
+
+import mcni
+
+class mcni_TestCase(unittest.TestCase):
+
+
+    def test(self):
+        from mcni.units import parser, mass
+        parser = parser()
+        self.assertAlmostEqual(parser.parse('u')/mass.kg, 1.66054e-27)
+        return
+    
+        
+    pass  # end of mcni_TestCase
+
+
+
+def pysuite():
+    suite1 = unittest.makeSuite(mcni_TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+def main():
+    #debug.activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    res = unittest.TextTestRunner(verbosity=2).run(alltests)
+    import sys; sys.exit(not res.wasSuccessful())
+
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id: mcni_TestCase.py 1126 2011-04-10 03:05:40Z linjiao $"
+
+# End of file 
