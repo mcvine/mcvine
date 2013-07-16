428a30c3f582e7e69a350a580543dea32e958b55
Jiao Lin <linjiao@caltech.edu>
2013-07-16 15:18:08 +0000

    mccomponents/sample/phonon/xml/parser/LinearlyInterpolatedDOS.py: attempt to add support for ascii file for DOS data
    git-svn-id: svn://danse.us/MCViNE/trunk@1599 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/mccomponents/sample/phonon/xml/parser/LinearlyInterpolatedDOS.py
+++ packages/mccomponents/mccomponents/sample/phonon/xml/parser/LinearlyInterpolatedDOS.py
@@ -24,6 +24,7 @@ class LinearlyInterpolatedDOS(AbstractNode):
     def elementFactory( self, **kwds ):
         idfpath = kwds.get('idf-data-path')
         h5path = kwds.get('histogram-path')
+        asciipath = kwds.get('ascii-path')
         
         if idfpath:
             from mccomponents.sample.phonon import dos_fromidf
@@ -31,6 +32,9 @@ class LinearlyInterpolatedDOS(AbstractNode):
         elif h5path:
             from mccomponents.sample.phonon import dos_fromh5
             return dos_fromh5(h5path)
+        elif asciipath:
+            from mccomponents.sample.phonon import dos_fromascii
+            return dos_fromascii(h5path)
         else:
             raise ValueError, "LinearlyInterpolatedDOS needs path to "\
                 "idf or histogram data file"            
