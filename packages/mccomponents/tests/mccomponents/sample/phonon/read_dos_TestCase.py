76b062ce65571a7121fffd0b9662ce6a8bf3edfc
Jiao Lin <linjiao@caltech.edu>
2013-07-30 18:12:33 +0000

    test of mccomponents.sample.phonon.read_dos
    git-svn-id: svn://danse.us/MCViNE/trunk@1619 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ packages/mccomponents/tests/mccomponents/sample/phonon/read_dos_TestCase.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2007 All Rights Reserved  
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+
+import unittestX as unittest
+import journal
+
+#debug = journal.debug( "TestCase" )
+#warning = journal.warning( "TestCase" )
+
+
+import mcni
+from mccomponents.sample.phonon import read_dos
+import histogram as H
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        "dos from ascii"
+        p = 'V-dos.dat'
+        dos = read_dos.doshist_fromascii(p)
+        # H.plot(dos)
+        read_dos.dos_fromascii(p)
+        return
+        
+        
+    def test2(self):
+        "dos from idf"
+        p = 'V-DOS.idf'
+        dos = read_dos.doshist_fromidf(p)
+        # H.plot(dos)
+        read_dos.dos_fromidf(p)
+        return
+        
+        
+    pass  # end of TestCase
+
+
+def pysuite():
+    suite1 = unittest.makeSuite(TestCase)
+    return unittest.TestSuite( (suite1,) )
+
+
+def main():
+    #debug.activate()
+    #journal.debug('phonon_coherent_inelastic_polyxtal_kernel').activate()
+    #journal.debug('random').activate()
+    pytests = pysuite()
+    alltests = unittest.TestSuite( (pytests, ) )
+    res = unittest.TextTestRunner(verbosity=2).run(alltests)
+    import sys; sys.exit(not res.wasSuccessful())
+
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id: dispersion_TestCase.py 1126 2011-04-10 03:05:40Z linjiao $"
+
+# End of file 
