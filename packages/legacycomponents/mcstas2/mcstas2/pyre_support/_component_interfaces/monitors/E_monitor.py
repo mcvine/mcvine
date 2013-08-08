2dbb77272a20692ed3d966648355e3673f832a0b
Jiao Lin <linjiao@caltech.edu>
2013-08-08 22:32:32 +0000

    pyre_support/_component_interfaces/monitors/E_monitor.py: fixed a bug in computing bins
    git-svn-id: svn://danse.us/MCViNE/trunk@1635 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/E_monitor.py
+++ packages/legacycomponents/mcstas2/mcstas2/pyre_support/_component_interfaces/monitors/E_monitor.py
@@ -28,7 +28,7 @@ def get_histogram(monitor):
     E2arr = bpptr2npyarr( core.getE_p2( ), 'double', n ).copy()
     from histogram import histogram, axis, arange
     dE = (core.Emax-core.Emin)/core.nchan
-    Eaxis = axis( 'energy', arange( core.Emin+dE/2, core.Emax+dE/2, dE ), unit = 'meV' )
+    Eaxis = axis( 'energy', arange( core.Emin+dE/2, core.Emax, dE ), unit = 'meV' )
     h = histogram( 'I(E)', [Eaxis], data = Iarr, errors = E2arr )
     return h
 
