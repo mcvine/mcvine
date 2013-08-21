626b7dbc27ea49178e6f4734fe8d31a295cf9dd5
Jiao Lin <linjiao@caltech.edu>
2013-08-21 19:44:16 +0000

    mcvine/bin/mcvine-convert-mcstas-instrument: config-<instrument> script generated from mcstas instrument is now pyre app with parameters in inventory
    git-svn-id: svn://danse.us/MCViNE/trunk@1659 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mcvine/tests/bin/convert-mcstas-instrument/test.sh
+++ packages/mcvine/tests/bin/convert-mcstas-instrument/test.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
+rm -rf out
+
 mcvine-convert-mcstas-instrument -input=dummy.instr
-./config-dummy
+./config-dummy --Emin=30.5
 ./dummy
 
 mcvine-convert-mcstas-instrument -input=dummy2.instr
-./config-dummy2
+./config-dummy2 --Edes=100 --E_min=50 --E_max=200 --Heusler
 ./dummy2
