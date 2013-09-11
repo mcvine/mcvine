dbd6cc5c0f525515f03462d07fec61a9d977f4a5
Jiao Lin <linjiao@caltech.edu>
2013-09-11 21:53:19 +0000

    tests/libmccomponents/homogeneous_scatterer/testCompositeScatteringKernel.cc: fixed assertions for accumulated probability in kernels
    git-svn-id: svn://danse.us/MCViNE/trunk@1684 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- packages/mccomponents/tests/libmccomponents/homogeneous_scatterer/testCompositeScatteringKernel.cc
+++ packages/mccomponents/tests/libmccomponents/homogeneous_scatterer/testCompositeScatteringKernel.cc
@@ -27,7 +27,9 @@ public:
     : absorbed(0),
       scattered(0),
       m_mu(mu), 
-      m_sigma(sigma)
+      m_sigma(sigma),
+      n_absorbed(0),
+      n_scattered(0)
   {}
   virtual double absorption_coefficient( const mcni::Neutron::Event & ev )
   {
@@ -39,14 +41,15 @@ public:
   }
   virtual void scatter( mcni::Neutron::Event & ev ) 
   {
-    scattered += ev.probability;
+    scattered += ev.probability; n_scattered ++;
     return;
   }
   virtual void absorb( mcni::Neutron::Event & ev ) 
   {
-    absorbed += ev.probability;
+    absorbed += ev.probability; n_absorbed ++;
   }
   double absorbed, scattered, m_mu, m_sigma;
+  int n_scattered, n_absorbed;
 };
 
 
@@ -55,7 +58,7 @@ void test1()
 {
   using namespace mccomponents;
 
-  Kernel1 kernel1(1,1), kernel2(2,2);
+  Kernel1 kernel1(2,1), kernel2(2,2);
   CompositeScatteringKernel::kernels_t kernels;
   kernels.push_back( &kernel1 );
   kernels.push_back( &kernel2 );
@@ -67,7 +70,7 @@ void test1()
   
   event = save;
   
-  assert (csk.absorption_coefficient( event ) == 3);
+  assert (csk.absorption_coefficient( event ) == 2);
   assert (csk.scattering_coefficient( event ) == 3);
 
   size_t N = 10000;
@@ -76,17 +79,20 @@ void test1()
     csk.scatter( event );
   }
   
-  assert( std::abs(kernel1.scattered-N/3.) < std::sqrt(N) * 2 );
-  assert( std::abs(kernel2.scattered-N*2/3.) < std::sqrt(N) * 2 );
+  assert( std::abs(kernel1.scattered-N) < std::sqrt(N) * 2 );
+  assert( std::abs(kernel2.scattered-N) < std::sqrt(N) * 2 );
+  assert( std::abs(kernel1.n_scattered-N/2.) < std::sqrt(N) * 2 );
+  assert( std::abs(kernel2.n_scattered-N/2.) < std::sqrt(N) * 2 );
 
   for (size_t i=0; i<N; i++) {
     event = save;
     csk.absorb( event );
   }
   
-  assert( std::abs(kernel1.absorbed-N/3.) < std::sqrt(N) * 2 );
-  assert( std::abs(kernel2.absorbed-N*2/3.) < std::sqrt(N) * 2 );
-
+  assert( std::abs(kernel1.absorbed-N) < std::sqrt(N) * 2 );
+  assert( std::abs(kernel2.absorbed-N) < std::sqrt(N) * 2 );
+  assert( std::abs(kernel1.n_absorbed-N/2.) < std::sqrt(N) * 2 );
+  assert( std::abs(kernel2.n_absorbed-N/2.) < std::sqrt(N) * 2 );
 }
 
 
