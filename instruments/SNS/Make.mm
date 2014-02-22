f65b3d33cfdca6552318d82a841339292b6a3557
Jiao Lin <linjiao@caltech.edu>
2014-02-22 04:27:05 +0000

    added mcvine.instruments.SNS.applications.Neutrons2Events
    git-svn-id: svn://danse.us/MCViNE/trunk@1764 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ instruments/SNS/Make.mm
@@ -0,0 +1,50 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                    Jiao Lin
+#                        California Institute of Technology
+#                        (C) 2008-2014  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+PROJECT = mcvine/SNS
+
+
+PROJ_TIDY += *.log *.tmp
+
+# directory structure
+
+BUILD_DIRS = \
+	SNS \
+	applications \
+
+OTHER_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
+
+#--------------------------------------------------------------------------
+#
+
+all:
+	BLD_ACTION="all" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+docs::
+	BLD_ACTION="docs" $(MM) recurse
+
+# version
+# $Id$
+
+# End of file
---
+++ instruments/SNS/SNS/applications/Make.mm
@@ -0,0 +1,52 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                    Jiao Lin
+#                        California Institute of Technology
+#                        (C) 2008-2014  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+PROJECT = mcvine
+PACKAGE = instruments/SNS/applications
+
+
+BUILD_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS)
+
+#--------------------------------------------------------------------------
+#
+
+all: export
+	BLD_ACTION="all" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+
+
+#--------------------------------------------------------------------------
+#
+# export
+
+EXPORT_PYTHON_MODULES = \
+	Neutrons2Events.py \
+	__init__.py \
+
+
+export:: export-package-python-modules 
+	BLD_ACTION="export" $(MM) recurse
+
+
+include doxygen/default.def
+docs: export-doxygen-docs
+
+
+# version
+# $Id: Make.mm 1679 2013-09-06 20:33:47Z linjiao $
+
+# End of file
