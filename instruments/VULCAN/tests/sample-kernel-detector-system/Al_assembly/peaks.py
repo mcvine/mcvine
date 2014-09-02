986c194b485788ad860ae7fb8fb4fa57be3e2587
Jiao Lin <linjiao@caltech.edu>
2014-09-02 18:24:35 +0000

    VULCAN/tests/sample-kernel-detector-system/Al_assembly/peaks.py: added unitcell volume and cross section data
    git-svn-id: svn://danse.us/MCViNE/trunk@1862 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- instruments/VULCAN/tests/sample-kernel-detector-system/Al_assembly/peaks.py
+++ instruments/VULCAN/tests/sample-kernel-detector-system/Al_assembly/peaks.py
@@ -6,3 +6,18 @@ peaks = [
     Peak(q=2.69, F_squared=1.69, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
     Peak(q=3.10, F_squared=1.69, multiplicity=6, intrinsic_line_width=0, DebyeWaller_factor=0),
     ]
+
+# added to include data regarding volume and cross sections
+# unit: \AA
+a = 4.049320
+unitcell_volume = a**3
+
+natoms = 4
+
+# unit: barns
+class cross_sections:
+    coh = natoms * 1.495
+    inc = natoms * 0.0082
+    abs = natoms * 0.231
+
+
