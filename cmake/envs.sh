bb75d2cd973f7bb862c6f8bb6dda29a7b7fc300e
Jiao Lin <linjiao@ornl.gov>
2015-11-16 14:13:55 -0500

    Refs #14. buildme and cmake/envs.sh: need to source danse

--- cmake/envs.sh
+++ cmake/envs.sh
@@ -1,4 +1,7 @@
+. /opt/danse/bin/setup-danse.sh
+
 EXPORT_ROOT=$PWD/build/export
 export PATH=$EXPORT_ROOT/bin:$PATH
 export PYTHONPATH=$EXPORT_ROOT/python:$PYTHONPATH
 export LD_LIBRARY_PATH=$EXPORT_ROOT/lib:$LD_LIBRARY_PATH
+export MCVINE_DIR=$EXPORT_ROOT
