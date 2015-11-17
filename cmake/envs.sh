2a625bb36566db8189a9b5486a086a113a19aecf
Jiao Lin <linjiao@ornl.gov>
2015-11-16 20:10:03 -0500

    Refs #14. tests passed. almost ready to merge. should modify travis ci config

--- cmake/envs.sh
+++ cmake/envs.sh
@@ -1,4 +1,8 @@
+# this script help set up the env vars for development
+# run this at the root of the mcvine source tree
+
 . /opt/danse/bin/setup-danse.sh
+export MCVINE_RESOURCES=~/dv/mcvine/resources
 
 EXPORT_ROOT=$PWD/build/export
 export PATH=$EXPORT_ROOT/bin:$PATH
