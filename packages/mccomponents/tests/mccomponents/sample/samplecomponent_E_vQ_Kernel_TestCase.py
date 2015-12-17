5537524af0103d2f43f4fc0f914893df1b87c37b
Jiao Lin <linjiao@ornl.gov>
2015-12-17 09:46:09 -0500

    added a test case for E_vQ_kernel to test discontinuity in dispersion surface

--- packages/mccomponents/tests/mccomponents/sample/samplecomponent_E_vQ_Kernel_TestCase.py
+++ packages/mccomponents/tests/mccomponents/sample/samplecomponent_E_vQ_Kernel_TestCase.py
@@ -45,7 +45,7 @@ class TestCase(unittest.TestCase):
         geometer.register( component1, (0,0,0), (0,0,0) )
         geometer.register( component2, (0,0,1), (0,0,0) )
         
-        N0 = 1000
+        N0 = 10000
         neutrons = mcni.neutron_buffer(N0)
         
         mcni.simulate( instrument, geometer, neutrons )
@@ -68,6 +68,63 @@ class TestCase(unittest.TestCase):
             self.assertAlmostEqual(E, E1)
             continue
 
+        return
+    
+
+    def test2(self):
+        'mccomponents.sample.samplecomponent: E_vQ_Kernel - discontinued dispersion surface'
+        import mcni
+        from mcni.utils import conversion
+        
+        ei = 60
+        vil = conversion.e2v(ei)
+        vi = (0,0,vil)
+        neutron = mcni.neutron( 
+            r = (0,0,0), v = vi, 
+            time = 0, prob = 1 )
+        
+        from mcni.components.MonochromaticSource import MonochromaticSource
+        component1 = MonochromaticSource('source', neutron)
+        
+        from mccomponents.sample import samplecomponent
+        component2 = samplecomponent( 'Al', 'sampleassemblies/Al-E_vQ_withdiscontinuity-kernel/sampleassembly.xml' )
+        E_Q = 'np.where(Qx*Qx+Qy*Qy+Qz*Qz>30, 25, 15)' # in sampleassemblies/Al-E_vQ-kernel/Al-scatterer.xml
+        
+        instrument = mcni.instrument( [component1, component2] )
+        
+        geometer = mcni.geometer()
+        geometer.register( component1, (0,0,0), (0,0,0) )
+        geometer.register( component2, (0,0,1), (0,0,0) )
+        
+        N0 = 10000
+        neutrons = mcni.neutron_buffer(N0)
+        
+        mcni.simulate( instrument, geometer, neutrons )
+        
+        N = len(neutrons)
+        
+        import numpy.linalg as nl
+        import numpy as np
+        from math import sin
+        N_noscatt = 0
+        for i in range(N):
+            neutron = neutrons[i]
+            vf = np.array(neutron.state.velocity)
+            diffv = vi - vf
+            # no scattering is fine
+            if np.linalg.norm(diffv) < 1e-12:
+                N_noscatt += 1
+                continue
+            Q = conversion.V2K * diffv
+            Qx, Qy, Qz = Q
+            ef = conversion.v2e(nl.norm(vf))
+            E = ei - ef
+            # print E, Q, neutron
+            E1 = eval(E_Q)
+            self.assertAlmostEqual(E, E1)
+            continue
+        print "\n* percentage of no scattering (Q happen to be at a singular point):",\
+            N_noscatt*100./N, "%"
         return
     
 
