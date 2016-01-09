e19c1f1130f18888da5c538772b973754cba4cf0
Jiao Lin <linjiao@ornl.gov>
2016-01-08 21:06:24 -0500

    mcstas2/python/mcstas2/release.py: more flexible in searching for mcstas component library

--- packages/legacycomponents/mcstas2/python/mcstas2/release.py
+++ packages/legacycomponents/mcstas2/python/mcstas2/release.py
@@ -18,7 +18,18 @@ from mcvine.deployment_info import mcvinedir, type
 
 component_library_dir = os.environ.get('MCSTAS_COMPONENT_LIBDIR', None)
 if component_library_dir is None:
-    component_library_dir = os.path.join(mcvinedir, 'share', 'mcstas2', 'McStas-Components')
+    candidates = [
+        os.path.join(mcvinedir, 'share', 'mcstas2', 'McStas-Components'),
+        os.path.join(mcvinedir, 'share', 'mcvine', 'mcstas2', 'McStas-Components'),
+        ]
+    for c in candidates:
+        if os.path.exists(c):
+            component_library_dir = c
+            break
+        continue
+    if not os.path.exists(component_library_dir):
+        raise RuntimeError("Cannot find McStas component library in %s" % (candidates,))
+        
 
     
 # version
