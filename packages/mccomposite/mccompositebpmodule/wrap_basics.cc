a427fe15f1fdad5cc08d079446c79be24bee7e2e
Jiao Lin <jiao.lin@gmail.com>
2016-01-18 07:59:41 -0500

    Refs #29. fixing missing symbols from mccompositebp

--- packages/mccomposite/mccompositebpmodule/wrap_basics.cc
+++ packages/mccomposite/mccompositebpmodule/wrap_basics.cc
@@ -19,6 +19,8 @@
 namespace wrap_mccomposite {
 
   typedef mccomposite::geometry::Vector Vector;
+  typedef mccomposite::geometry::RotationMatrix RotationMatrix;
+  
   inline double V3_getitem( const Vector & v3, size_t i)
   {
     return v3[i];
@@ -33,26 +35,29 @@ namespace wrap_mccomposite {
   {
     using namespace boost::python;
     // check if prior registration exists
-    type_info info = type_id<Vector>();
-    if (converter::registry::query(info)!=NULL) return;
-    // register
-    class_<Vector>
-      ("Vector",
-       init<double, double, double>()
-       )
-      .def(init<> () )
-      .def("__len__", &V3_size )
-      .def("__getitem__", &V3_getitem)
-      .def("__iter__", iterator<Vector>() )
-      ;
+    if (converter::registry::query(type_id<Vector>())==NULL) {
+      // register
+      class_<Vector>
+	("Vector",
+	 init<double, double, double>()
+	 )
+	.def(init<> () )
+	.def("__len__", &V3_size )
+	.def("__getitem__", &V3_getitem)
+	.def("__iter__", iterator<Vector>() )
+	;
+    }
 
-    class_<mccomposite::geometry::RotationMatrix>
-      ("RotationMatrix",
-       init<double, double, double, double, double, double, double, double, double>()
-       )
-      .def(init<> () )
-      ;
-  }
+    // check if prior registration exists
+    if (converter::registry::query(type_id<RotationMatrix>())==NULL) {
+      class_<RotationMatrix>
+	("RotationMatrix",
+	 init<double, double, double, double, double, double, double, double, double>()
+	 )
+	.def(init<> () )
+	;
+    }
+  } // wrap_basics
 }
 
 
