ce0089682210284a921d3e900468251ab6be298a
Jiao Lin <linjiao@ornl.gov>
2016-02-02 13:46:37 -0500

    Refs #32. fixing "mcvine sampleassembly"

--- packages/mcvine/python/mcvine/cli/sampleassembly/check.py
+++ packages/mcvine/python/mcvine/cli/sampleassembly/check.py
@@ -1,52 +1,18 @@
 # -*- Python -*-
 #
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-#                                   Jiao Lin
-#                      California Institute of Technology
-#                      (C) 2006-2013  All Rights Reserved
-#
-# {LicenseText}
-#
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Jiao Lin <jiao.lin@gmail.com>
 #
 
+import os, click
 
-from .. import name as cliname
-from . import name as cmdname
-actionname = 'check'
-
-import os
-def run(xml, **kwds):
+from . import sampleassembly
+@sampleassembly.command()
+@click.argument("xml")
+def check(xml):
     # import mccomponents.sample.phonon.xml
     from mccomponents.sample import samplecomponent
     samplecomponent("s", xml)
     return
 
-
-def parse_cmdline():
-    print ("%s %s %s: check a sample assembly\n" % (cliname, cmdname, actionname))
-    
-    import optparse
-    usage = "usage: %prog " + cmdname + " " + actionname + " xmlpath [options]"
-    
-    parser = optparse.OptionParser(usage, add_help_option=True)
-    
-    #
-    # parser.add_option('-p', '--port', type="int", default=18861, dest='port')
-    
-    #
-    options, args = parser.parse_args()
-    if len(args) > 3:
-        parser.error("too many arguments\n\n")
-    if len(args) == 2:
-        parser.error("please specify path to sample assembly xml file")
-        
-    args, kwds = args[2:], vars(options)
-    return args, kwds
-
-
-# version
-__id__ = "$Id$"
 
 # End of file 
