f403a0afbe5b8d346540a157688d93c3a5ba156c
Jiao Lin <linjiao@ornl.gov>
2016-02-15 15:15:08 -0500

    packages/mcvine/python/mcvine/__init__.py: minor

--- packages/mcvine/python/mcvine/__init__.py
+++ packages/mcvine/python/mcvine/__init__.py
@@ -42,7 +42,7 @@ import yaml, os
 conf_path = "mcvine.conf"
 config = dict()
 if os.path.exists(conf_path):
-    config = yaml.load(open("mcvine.conf"))
+    config = yaml.load(open(conf_path))
 
 import logging.config
 logging_conf = config.get("logging")
