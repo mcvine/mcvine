3b4c308d552699fcd1110199bf84c3af8bb7e5af
Jiao Lin <linjiao@ornl.gov>
2016-02-18 09:09:43 -0500

    Refs #4. ARCS cli: added alias "arcs". added help text for subcmds

--- instruments/ARCS/ARCS/cli.py
+++ instruments/ARCS/ARCS/cli.py
@@ -7,12 +7,13 @@ import click
 from ..cli import instrument
 from mcvine.cli import pyre_app, alias
 
+cmd_prefix = "mcvine instrument arcs "
+
 @instrument.group()
+@alias("arcs", cmd_prefix)
 def arcs():
     return
 
-
-cmd_prefix = "mcvine instrument arcs "
 arcs_app = lambda name: pyre_app(parent=arcs, appname = name, cmd_prefix=cmd_prefix)
 
 # beam sim
@@ -23,22 +24,26 @@ def analyze_beam(ctx, appname):
 
 @arcs_app('arcs_moderator2sample')
 def mod2sample(ctx, appname):
+    "moderator to sample simulation"
     from .applications.Moderator2Sample import App
     return App(appname)
 
 @arcs_app('arcs_m2s')
 def m2s(ctx, appname):
+    "simplified moderator to sample simulation app"
     from .applications.M2S import App
     return App(appname)
 
 @arcs_app('arcs_beam')
 def beam(ctx, appname):
+    "beam simulation. include mod2sample sim and post-processing"
     from .applications.Beam import App
     return App(appname)
 
 
 # detsys sim
-@arcs.command()
+@arcs.command(help="""convert scattereed neutrons to events (pixelID, tofChannelNo, prob)
+intercepted by ARCS detector system.""")
 @click.argument("neutrons", default="neutrons.dat")
 @click.option("--workdir", default='work-arcs-neutrons2events')
 @click.option("--nodes", default=0)
