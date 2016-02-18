df9ba200bb9e82f5141b19da16fe0af9559928b9
Jiao Lin <linjiao@ornl.gov>
2016-02-18 15:17:06 -0500

    Refs #4. ARCS/beam_postprocessing.py: fixed call to "mcvine instruments arcs m2s"

--- instruments/ARCS/ARCS/beam_postprocessing.py
+++ instruments/ARCS/ARCS/beam_postprocessing.py
@@ -98,7 +98,7 @@ def runMonitorsAtSample(E, m2sout, out):
     from mcni.neutron_storage.idf_usenumpy import count
     n = count(neutronfile)
 
-    cmd = ['mcvine instrument arcs analyze_beam']
+    cmd = ['mcvine instruments arcs analyze_beam']
     cmd += ['--output-dir=%s' % out]
     cmd += ['--ncount=%s' % n]
     cmd += ['--buffer_size=%s' % min(n, 1e6)]
