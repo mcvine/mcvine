f897ec8f0197c304c0cb65e487f11f753e976827
Jiao Lin <linjiao@ornl.gov>
2016-02-19 11:38:59 -0500

    Refs #31. test passed in dev

--- instruments/ARCS/tests/workflows/Al-300K/Workflow_TestCase.py
+++ instruments/ARCS/tests/workflows/Al-300K/Workflow_TestCase.py
@@ -4,16 +4,25 @@
 #
 
 import mcvine, mcvine.resources
-import os, shutil
+import os, shutil, subprocess as sp
 
 import unittest
 class TestCase(unittest.TestCase):
 
     def test(self):
         "ARCS Al 300K simulation workflow"
+        # copy template
         src = os.path.join(mcvine.resources.instrument('ARCS'), 'simulations/aluminum/plate-rotated28deg-Ei80meV')
-        dest = os.path.join("work")
+        workdir = "work"
+        dest = os.path.join(workdir)
+        shutil.rmtree(dest)
         shutil.copytree(src, dest)
+        # run 
+        from mcvine.instruments.ARCS.applications.utils import execute
+        execute("make", workdir)
+        # generate a plot
+        cmd = "PlotHist.py --min=0 --max=0.001 --output=iqe.eps %s/iqe.h5" % workdir
+        execute(cmd, os.curdir)
         return
 
 
