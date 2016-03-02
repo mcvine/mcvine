08f012c3c1fe78107df9217cad2ad8debdecda4e
Jiao Lin <linjiao@ornl.gov>
2016-03-02 10:31:56 -0500

    Refs #61. added options --cartesian and --output to cmd "mcvine phonon band"

--- packages/mcvine/python/mcvine/cli/phonon.py
+++ packages/mcvine/python/mcvine/cli/phonon.py
@@ -15,7 +15,9 @@ def phonon():
 @click.option("--start", default=(0.,0.,0.))
 @click.option("--end", default=(0.,0.,1.))
 @click.option("--npts", default=100)
-def band(phonon, start, end, npts):
+@click.option("--cartesian", default=False, is_flag=True)
+@click.option("--output", default="")
+def band(phonon, start, end, npts, cartesian, output):
     "Plot band structure along one direction"
     # phonon is the path to a directory with IDF phonon data
     from mccomponents.sample.phonon import periodicdispersion_fromidf as pd
@@ -24,10 +26,23 @@ def band(phonon, start, end, npts):
     import mccomponents.homogeneous_scatterer as mh
     cdisp = mh.scattererEngine(disp)
     Qs = np.array(start) + (np.array(end)-np.array(start)) * np.arange(0, 1, 1./npts)[:, np.newaxis]
+    if not cartesian:
+        # read reciprocal basis vectors from Qgridinfo
+        import os
+        lines = open(os.path.join(phonon, 'Qgridinfo')).readlines(3)
+        d = {}
+        for line in lines:
+            exec(line, d)
+            continue
+        reci_basis = [d['b1'], d['b2'], d['b3']]
+        Qs = np.dot(Qs, reci_basis)
     Es = [cdisp.energy(0, mcni.vector3(*Q)) for Q in Qs]
     import pylab
     pylab.plot(Es)
-    pylab.show()
+    if output:
+        pylab.savefig(output)
+    else:
+        pylab.show()
     return
 
 
