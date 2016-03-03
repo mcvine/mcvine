2dc432122add7dfa52b9e870810d85e142769957
Jiao Lin <linjiao@ornl.gov>
2016-03-03 09:14:51 -0500

    added builders/sim-travis.sh

---
+++ builders/sim-travis.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# simulate the travis environment
+# run under ~/dv/mcvine/travis-CI/mcvine
+# ./builders/sim-travis.sh <branch>
+
+# update and pull the branch
+git checkout master
+git pull
+git checkout $1
+git pull
+
+# update resources
+cd resources
+git pull
+cd ..
+
+echo "************************************************************************"
+echo "*** Will start docker image. "
+echo
+echo Inside the docker, run
+echo
+echo  $ ./builders/build /mcvine-src /mcvine-build /mcvine-install /mcvine-src/resources
+echo
+echo to build.
+echo
+echo Then run
+echo
+echo  $ export MCVINE_RESOURCES=/mcvine-src/resources 
+echo  $ . /opt/danse/bin/setup-danse.sh 
+echo  $ cd /mcvine-build/
+echo
+echo to get ready for testing.
+echo
+echo Run
+echo
+echo  $ ctest -R testname -V
+echo
+echo to test
+echo "************************************************************************"
+
+# start the docker image
+docker run -it -v $PWD:/mcvine-src -w /mcvine-src linjiao/mcvine-ubuntu-deps:14.04 bash
+
+
