15a846672fe3e90bb8d330c1183157162a0b58f6
Jiao Lin <linjiao@ornl.gov>
2016-03-03 09:51:42 -0500

    Refs #61. some inline docs

--- packages/mcvine/python/mcvine/cli/phonon.py
+++ packages/mcvine/python/mcvine/cli/phonon.py
@@ -21,23 +21,30 @@ def phonon():
 def band(phonon, start, end, npts, cartesian, output, branch):
     "Plot band structure along one direction"
     # phonon is the path to a directory with IDF phonon data
+
+    # read phonon data
     from mccomponents.sample.phonon import periodicdispersion_fromidf as pd
     import mcni, numpy as np
     disp = pd(phonon)
+    # and construct the proxy to the c++ data object
     import mccomponents.homogeneous_scatterer as mh
     cdisp = mh.scattererEngine(disp)
+    # create Q array
     Qs = np.array(start) + (np.array(end)-np.array(start)) * np.arange(0, 1, 1./npts)[:, np.newaxis]
+    # Q will be cartesian
     if not cartesian:
         # read reciprocal basis vectors from Qgridinfo
         import os
         reci_basis = recibasis_fromQgridinfo(os.path.join(phonon, 'Qgridinfo'))
         Qs = np.dot(Qs, reci_basis)
+    # compute energies
     nbr = disp.dispersion.nBranches
     Es = [cdisp.energy(br, mcni.vector3(*Q))
           for Q in Qs
           for br in range(nbr)]
     Es = np.array(Es)
     Es.shape = -1, nbr
+    # output
     try:
         import pylab
     except ImportError:
@@ -68,16 +75,20 @@ def band(phonon, start, end, npts, cartesian, output, branch):
 def slice(crystal, phonon, start, end, npts, cartesian, outhist, eaxis):
     "Plot slice of SQE data along a specific reciprocal space direction"
     # phonon is the path to a directory with IDF phonon data
+
+    # read phonon data
     from mccomponents.sample.phonon import periodicdispersion_fromidf as pd
     import mcni, numpy as np
     disp = pd(phonon)
+    # and construct a proxy to the c++ data object
     import mccomponents.homogeneous_scatterer as mh
     cdisp = mh.scattererEngine(disp)
+    # create Q array
     start = np.array(start)
     end = np.array(end)
     step = (end-start)/npts
     Qs = start + step * np.arange(0, npts, 1.)[:, np.newaxis]
-
+    # Qs: cartesian, hkls: miller indexes
     import os
     reci_basis = recibasis_fromQgridinfo(os.path.join(phonon, 'Qgridinfo'))
     inv_reci_basis = np.linalg.inv(reci_basis)
@@ -116,19 +127,21 @@ def slice(crystal, phonon, start, end, npts, cartesian, outhist, eaxis):
     parser = getParser(os.path.splitext(crystal)[-1][1:])
     structure = parser.parseFile(crystal)
     atom_positions = [atom.xyz for atom in structure]
-    
+    # create "events" for later histogramming process to construct slice
     events = computeEvents(hkls, Es, pols, atom_positions, reci_basis)
-
+    # for x axis of the histogram (along Q)
     maxx = np.linalg.norm(end-start)
     xaxis = 0, maxx, maxx/npts
     # Eaxis = 0, np.max(Es) * 1.1, 1.
     Eaxis = eaxis
-
+    # functor to convert hkl to "x" value
     def Qtox(hkl):
         x = np.linalg.norm(hkl-start, axis=-1)
         mask = x==x
         return x, mask
+    # histogramming
     h = makeSlice(events, xaxis, Eaxis, Qtox)
+    # output
     import histogram.hdf as hh
     hh.dump(h, outhist)
     return
@@ -147,7 +160,10 @@ def recibasis_fromQgridinfo(path):
 
 
 def computeEvents(Qs, omegas, pols, atom_positions, rec_lattice):
-    """
+    """Given phonon data, generate events of (vector Q, E, and neutron scattering intensities)
+    these events can be histogrammed into slice view of neutron scattering data.
+    see below function makeSlice
+
     Qs, omegas, pols are numpy arrays
     They are generated from band.yaml.
     
@@ -181,7 +197,8 @@ def computeEvents(Qs, omegas, pols, atom_positions, rec_lattice):
 
 
 def makeSlice(events, xaxis, Eaxis, Qtox):
-    """
+    """convert input events into a histogram (slice)
+    
     Qtox:
     convert Q to x. also return a mask. since some Q points should be
     discarded
