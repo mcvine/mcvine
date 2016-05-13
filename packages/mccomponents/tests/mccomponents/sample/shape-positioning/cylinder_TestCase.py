4e124aebd811b6490fe34ff25c3fd77807bdd632
Jiao Lin <linjiao@ornl.gov>
2016-05-13 13:52:40 -0400

    Refs #125. first test case implemented: cylinder

--- packages/mccomponents/tests/mccomponents/sample/shape-positioning/cylinder_TestCase.py
+++ packages/mccomponents/tests/mccomponents/sample/shape-positioning/cylinder_TestCase.py
@@ -3,7 +3,7 @@
 #
 
 
-import unittest
+import unittest, numpy as np
 
 
 class TestCase(unittest.TestCase):
@@ -11,8 +11,31 @@ class TestCase(unittest.TestCase):
 
     def test1(self):
         'shape positioning: cylinder'
+        # source
+        from mcni.components.MonochromaticSource import MonochromaticSource
+        import mcni
+        neutron = mcni.neutron(r=(0,0,-1), v=(0,0,1000), prob=1)
+        source = MonochromaticSource('s', neutron, dx=0.012, dy=0.07, dE=0)
+        # sample
         from mccomponents.sample import samplecomponent
         scatterer = samplecomponent('sa', 'cyl/sampleassembly.xml' )
+        # neutrons
+        N = 1000
+        neutrons = mcni.neutron_buffer(N)
+        neutrons = source.process(neutrons)
+        # find neutrons out of target
+        arr = neutrons.to_npyarr()
+        x,y,z = arr[:, :3].T
+        missing = (x>0.01) | (x<-0.01) | (y>0.03) | (y<-0.03)
+        # print neutrons
+        scatterer.process(neutrons)
+        # print neutrons
+        arr = neutrons.to_npyarr()
+        x,y,z = arr[:, :3].T
+        assert (z[missing] < -.9).all()
+        hit = arr[np.logical_not(missing), :3]
+        x,y,z = hit.T
+        assert (np.isclose((x*x + z*z)**.5, 0.01) | np.isclose(np.abs(y), 0.03)).all()
         return
     
 
