fe999d0f3a73f852ab6fddf2a4e1dba64601af34
Jiao Lin <jiao.lin@gmail.com>
2016-05-15 09:10:24 -0400

    Refs #125. trivial update of docstr

--- packages/mccomponents/tests/mccomponents/sample/shape-positioning/hollowCylinder_TestCase.py
+++ packages/mccomponents/tests/mccomponents/sample/shape-positioning/hollowCylinder_TestCase.py
@@ -10,7 +10,7 @@ class TestCase(unittest.TestCase):
 
 
     def test1(self):
-        'shape positioning: cylinder'
+        'shape positioning: hollow cylinder'
         # source
         from mcni.components.MonochromaticSource import MonochromaticSource
         import mcni
