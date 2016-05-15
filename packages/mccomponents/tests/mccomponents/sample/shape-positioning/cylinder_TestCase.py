52bdb4e817be157e4e0cbda8eb08077704acac29
Jiao Lin <jiao.lin@gmail.com>
2016-05-15 09:18:00 -0400

    Refs #125. added a test case: cylinder axis along beam

--- packages/mccomponents/tests/mccomponents/sample/shape-positioning/cylinder_TestCase.py
+++ packages/mccomponents/tests/mccomponents/sample/shape-positioning/cylinder_TestCase.py
@@ -10,7 +10,7 @@ class TestCase(unittest.TestCase):
 
 
     def test1(self):
-        'shape positioning: cylinder'
+        'shape positioning: cylinder with vertical axis'
         # source
         from mcni.components.MonochromaticSource import MonochromaticSource
         import mcni
@@ -36,6 +36,37 @@ class TestCase(unittest.TestCase):
         hit = arr[np.logical_not(missing), :3]
         x,y,z = hit.T
         assert (np.isclose((x*x + z*z)**.5, 0.01) | np.isclose(np.abs(y), 0.03)).all()
+        return
+    
+
+    def test2(self):
+        'shape positioning: cylinder with axis along beam'
+        # source
+        from mcni.components.MonochromaticSource import MonochromaticSource
+        import mcni
+        neutron = mcni.neutron(r=(0,0,-1), v=(0,0,1000), prob=1)
+        source = MonochromaticSource('s', neutron, dx=0.09, dy=0.09, dE=0)
+        # sample
+        from mccomponents.sample import samplecomponent
+        scatterer = samplecomponent('sa', 'cyl-along-beam/sampleassembly.xml' )
+        # neutrons
+        N = 1000
+        neutrons = mcni.neutron_buffer(N)
+        neutrons = source.process(neutrons)
+        # find neutrons out of target
+        arr = neutrons.to_npyarr()
+        x,y,z = arr[:, :3].T
+        missing = x*x+y*y > 0.04**2
+        # print neutrons
+        scatterer.process(neutrons)
+        # print neutrons
+        arr = neutrons.to_npyarr()
+        x,y,z = arr[:, :3].T
+        assert (z[missing] < -.9).all()
+        hit = arr[np.logical_not(missing), :3]
+        x,y,z = hit.T
+        assert (z > -.1).all()
+        assert (np.isclose((x*x + y*y)**.5, 0.04) | np.isclose(np.abs(z), 0.005)).all()
         return
     
 
