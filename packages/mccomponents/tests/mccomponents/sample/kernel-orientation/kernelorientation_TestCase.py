e289735f3ef75f6f554da5b1b476cd3ec7fedf82
Jiao Lin <linjiao@ornl.gov>
2016-05-16 12:54:19 -0400

    Refs #124. added docstr for the test

--- packages/mccomponents/tests/mccomponents/sample/kernel-orientation/kernelorientation_TestCase.py
+++ packages/mccomponents/tests/mccomponents/sample/kernel-orientation/kernelorientation_TestCase.py
@@ -1,6 +1,52 @@
 #!/usr/bin/env python
 #
-#
+# Jiao Lin <jiao.lin@gmail.com
+
+"""
+This test check the "orientation" parameter of kernels.
+
+* Sub-kernels in a "KernelContainer" have the parameter "orientation"
+  to specify its orientation relative to its parent kernel.
+* The root level KernelContainer always has the same coordinate system
+  as the scatterer.
+
+
+In this test the coordinate system of the kernel
+is rotated 30 deg around the y axis (vertical up)
+with respect to the scatterer.
+Roughtly it is illustrated below:
+
+x'        ^ x
+   |\     |
+     \    |
+      \   |            >  z'
+       \  |        . '
+        \ |    . '
+         \|. ' )  30 deg
+          -------------------> z
+
+So the transformation matrix is
+
+sqrt(3)/2    0    1/2
+   0         1     0
+  -1/2       0    sqrt(3)/2
+
+This is specified in cyl/X-scatterer.xml.
+
+In kernel's coordinate system, we set the momentum transfer
+of the kernel to be [2,0,0], which is is along x' axis.
+
+The incident neutron is along z axis with energy 100meV.
+
+With these information, we can compute the momentum transfer
+in instrument cooridnate system, and then the final energy
+and energy transfer E. 
+Turns out E = -37.07822meV, and this is set in cyl/X-scatterer.xml.
+
+In the following test, we make sure the final velocities of 
+the scattered neutrons are expected, and the neutrons
+have valid probabilities.
+"""
 
 
 import unittest, numpy as np
@@ -43,6 +89,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(len(neutrons), N)
         for neutron in neutrons:
             np.allclose(neutron.state.velocity, vf)
+            self.assert_(neutron.probability > 0)
             continue
         return
     
