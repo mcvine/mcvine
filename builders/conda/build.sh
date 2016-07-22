ef6c0d43bd7cce339da92397abcf1b65596da62f
Jiao Lin <linjiao@ornl.gov>
2016-07-22 11:45:45 -0400

    Refs #149. restoring the correct cmake cmd combination in builders/conda/build.sh

--- builders/conda/build.sh
+++ builders/conda/build.sh
@@ -10,5 +10,5 @@ PYVER=${PYVER_MAJOR}.${PYVER_MINOR}
 
 mkdir build
 cd build
-# cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PYVER}.so -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PYVER} .. && make -j $CORES && make -j$CORES reconfigure-to-include-mcstas-components && make -j$CORES wrap-mcstas-components-cmake && make -j$CORES && make install
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PYVER}.so -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PYVER} .. && make -j $CORES && make install
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PYVER}.so -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PYVER} .. && make -j $CORES && make -j$CORES reconfigure-to-include-mcstas-components && make -j$CORES wrap-mcstas-components-cmake && make -j$CORES && make install
+# cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PYVER}.so -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PYVER} .. && make -j $CORES && make install
