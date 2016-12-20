b1cd19a58d205c0cd5155b28e2bb15951122d61b
Jiao Lin <linjiao@ornl.gov>
2016-12-20 15:21:50 -0500

    Refs #131. added config.Struct.__setitem__

--- packages/mcvine/python/mcvine/cli/config.py
+++ packages/mcvine/python/mcvine/cli/config.py
@@ -13,20 +13,21 @@ def loadYmlConfig(path):
     f.close()
     return Struct(d)
 
-
 # This class was copied from a StackOverflow thread
 class Struct:
     '''The recursive class for building and representing objects with.'''
     def __init__(self, obj):
         for k, v in obj.items():
             setattr(self, k, _struct(v))
-    
     def __getitem__(self, val):
         return self.__dict__[val]
-  
+    def __setitem__(self, key, val):
+        self.__dict__[key] = val
+        return val
     def __repr__(self):
         return '{%s}' % str(', '.join('%s : %s' % (k, repr(v)) for
                                       (k, v) in self.__dict__.items()))
+
 def _struct(v):
     if isinstance(v, dict):
         return Struct(v)
