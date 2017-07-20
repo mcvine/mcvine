043fa5a1ae040550755233fe9d775d552beb61f9
Jiao Lin <linjiao@ornl.gov>
2017-07-20 16:10:17 -0400

    Refs #47. converted "extract" cmd

--- packages/mcvine/python/mcvine/cli/neutronstorage.py
+++ packages/mcvine/python/mcvine/cli/neutronstorage.py
@@ -18,4 +18,26 @@ def count(path):
     return
 
 
+@neutronstorage.command()
+@click.argument("inputpath")
+@click.argument("outputpath")
+@click.option("--start", default=0, help='start index')
+@click.option("--end", default=None, type=int, help='stop index')
+def extract(inputpath, outputpath, start, end):
+    if start >= end:
+        raise ValueError, "Not a valid range: %s, %s" % (
+            start, end)
+
+    n = end - start
+
+    from mcni.neutron_storage.idf_usenumpy import read, write
+    # read neutrons
+    neutrons = read(inputpath, start=start, n = n)
+    # write them
+    write(neutrons, filename=outputpath)
+    return
+
+
+
+
 # End of file 
--- packages/mcvine/tests/bin/cli/neutronstorage/TestCase.py
+++ packages/mcvine/tests/bin/cli/neutronstorage/TestCase.py
@@ -13,5 +13,14 @@ class TestCase(unittest.TestCase):
             raise RuntimeError("%s failed" % cmd)
         return
 
+    def test_extract(self):
+        "mcvine neutronstorage extract"
+        cmd = "mcvine neutronstorage extract neutrons neutrons2 --start 0 --end 10"
+        if os.system(cmd):
+            raise RuntimeError("%s failed" % cmd)
+        from mcni.neutron_storage.idf_usenumpy import count
+        assert count('neutrons2')==10
+        return
+
 
 if __name__ == '__main__': unittest.main()
