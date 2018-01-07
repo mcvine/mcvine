mcvine/mcvine

commit 173abe74924e14be919fa4e8283057568f38aec2

Refs #157. create the temp folder of the harness applications in curdir so it is easy to debug

packages/mcvine/python/mcvine/applications/InstrumentBuilder.py
@@ -27,7 +27,7 @@ def build(neutron_components):
         def run(self, *args, **kwds):
             # create a temp work dir
             import tempfile
-            workdir = tempfile.mkdtemp()
+            workdir = tempfile.mkdtemp(dir = os.path.abspath(os.curdir), prefix='simapp-')
             # create an application script
             apppath = os.path.join(workdir, 'simapp.py')
             appscript = """from mcvine.applications.InstrumentBuilder import _build
@@ -44,7 +44,8 @@ app.run(*args, **kwds)
             sysargs = ' '.join('"%s"' % a for a in sys.argv[1:])
             ppsd = os.path.join(workdir, 'post-processing-scripts')
             sysargs += ' --post-processing-scripts-dir=%s' % ppsd
-            cmd = '%s %s %s' % (sys.executable, apppath, sysargs)
+            logpath = os.path.join(workdir, 'log.sim')
+            cmd = '%s %s %s&>%s' % (sys.executable, apppath, sysargs, logpath)
             _exec(cmd)
             # run the postprocessing script
             from mcni.pyre_support.Instrument import _run_ppsd

commit fcd0d7cdfabada5975a8650895e925cd2c9e3581

Refs #157. fixing a bug introduced by 173abe7

packages/mcvine/python/mcvine/applications/InstrumentBuilder.py
@@ -39,13 +39,14 @@ app.run(*args, **kwds)
 """ % dict(neutron_components=neutron_components, 
            init_params = self._init_params,
            run_params = (args, kwds))
-            open(apppath, 'wt').write(appscript)
+            with open(apppath, 'wt') as ostream:
+                ostream.write(appscript)
             # run the script in a subprocess
             sysargs = ' '.join('"%s"' % a for a in sys.argv[1:])
             ppsd = os.path.join(workdir, 'post-processing-scripts')
             sysargs += ' --post-processing-scripts-dir=%s' % ppsd
             logpath = os.path.join(workdir, 'log.sim')
-            cmd = '%s %s %s&>%s' % (sys.executable, apppath, sysargs, logpath)
+            cmd = '%s %s %s>%s 2>&1' % (sys.executable, apppath, sysargs, logpath)
             _exec(cmd)
             # run the postprocessing script
             from mcni.pyre_support.Instrument import _run_ppsd