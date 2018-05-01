f1e2d3d3ded0ea053d6620e98ddbb0619e7ab79b
Jiao Lin <linjiao@ornl.gov>
2018-04-30 23:47:15 -0400

    Refs #290. made _proxies.base.__init__ compatible with two cases cf(name, **kwds) cf(**kwds)

--- packages/legacycomponents/mcstas2/python/mcstas2/components/_proxies/base.py
+++ packages/legacycomponents/mcstas2/python/mcstas2/components/_proxies/base.py
@@ -4,12 +4,15 @@ from mcni.components.ParallelComponent import ParallelComponent
 class Component(AbstractComponent, ParallelComponent):
 
     def __init__(self, cpp_instance_factory, *args, **kwds):
+        # args, if exists, must be "name"
+        assert len(args) in [0, 1]
+        if args: kwds['name' ] = args[0]
         for k, v in kwds.items():
             setattr(self, k, v)
         # self._cpp_instance is an instance created
         # by factory methods auto-generated from mcstas components
         # see template code in mcstas2.wrappers.pymodule.factorymethod_py
-        self._cpp_instance = cpp_instance_factory(*args, **kwds)
+        self._cpp_instance = cpp_instance_factory(**kwds)
         self.restore_neutron = False
         self._cpp_instance_factory = cpp_instance_factory
         return
