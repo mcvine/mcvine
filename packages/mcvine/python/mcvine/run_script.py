33f4501c93e1a5f82b5dfa161785df5d567c85fe
Jiao Lin <linjiao@ornl.gov>
2018-05-03 17:06:26 -0400

    Refs #292. run_script: added CLI support

--- packages/mcvine/python/mcvine/run_script.py
+++ packages/mcvine/python/mcvine/run_script.py
@@ -4,23 +4,31 @@
 
 import os, sys
 
-def mpi_run(script, ncount, buffer_size=int(1e6)):
+def mpi_run(script, workdir, ncount, nodes, buffer_size=int(1e6), overwrite_datafiles=False):
     """run a mcvine simulation script on one node. The script must define the instrument.
-An example script:
-
-# script mysim.py
-
-# end of script
-
 Parameters:
 
-* script: path to script
+* script: path to instrument script
+* workdir: working dir
 * ncount: neutron count
 * nodes:  number of MPI nodes
-* buffer_size: buffer size
-"""
-    raise NotImplementedError
+* buffer_size: buffer size (optional)
 
+An example script:
+
+  # script myinstrument.py
+  import mcvine
+  instrument = mcvine.instrument()
+  # add source
+  source = mcvine.components.sources.Source_simple('source')
+  instrument.append(source, position=(0,0,0))
+  # add monitor
+  monitor = mcvine.components.monitors.E_monitor('monitor', filename='IE.dat')
+  instrument.append(monitor, position=(0,0,1))
+  # end of script
+
+"""
+    return
 
 def run1(script, workdir, ncount, buffer_size=int(1e6), overwrite_datafiles=False):
     """run a mcvine simulation script on one node. The script must define the instrument.
@@ -69,3 +77,17 @@ An example script:
     from mcni import run_ppsd
     run_ppsd(ppsd)
     return
+
+
+import click
+@click.command()
+@click.argument("script")
+@click.option("--workdir", default="output", help="work directory")
+@click.option("--ncount", default=int(1e6), help="neutron count")
+@click.option("--overwrite_datafiles", default=False, help="overwrite datafiles", is_flag=True)
+@click.option("--buffer_size", default=int(1e6), help="neutron buffer size")
+def main(script, workdir, ncount, buffer_size=int(1e6), overwrite_datafiles=False):
+    run1(script, workdir, ncount, buffer_size, overwrite_datafiles)
+    return
+
+if __name__ == '__main__': main()
