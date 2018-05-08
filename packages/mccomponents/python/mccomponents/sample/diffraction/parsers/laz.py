d658ae0416db85453fe8d8552711caa5d9ae4f46
Garrett Granroth <granrothge@ornl.gov>
2018-05-08 11:33:57 -0400

    updated to remove fixed column labels, line position requriements, and work with F^2

--- packages/mccomponents/python/mccomponents/sample/diffraction/parsers/laz.py
+++ packages/mccomponents/python/mccomponents/sample/diffraction/parsers/laz.py
@@ -37,13 +37,26 @@ def parse(text, line_width=0, dw_factor=1):
     """parse given text of laz format and returns a list of peaks
     """
     # text.replace("\r", "")  # Clean from CR  # THIS LINE WAS NOT USEFUL
-    
-    lines   = text.splitlines()
-    comments = [l for l in lines if l.startswith('#')]
+
+    lines = text.splitlines()
+    Comments_dict = {}
+    for l in lines:
+         if l.startswith('#'):
+            tmplst = l.split()
+            if len(tmplst) > 1:
+                ky=tmplst[1].strip()
+                if ky.lower().startswith('atom'):
+                    ky=ky+'_'+tmplst[2]
+                if ky in Comments_dict.keys():
+                    raise RuntimeError ("Duplicate Comment %s in laz file" %(tmplst[1]))
+                Comments_dict[ky] = l
+                if 'nb_atoms' in ky:
+                    print ('\n key: '+ky+":"+Comments_dict[ky]+" \n")
+
     class laz:
-        lattice = getLattice(comments)
-        peaks = getPeaks(text, lattice, line_width=line_width, dw_factor=dw_factor)
-        cross_sections = getCrossSections(comments)
+        lattice = getLattice(Comments_dict['CELL'])
+        peaks = getPeaks(text, Comments_dict, lattice, line_width=line_width, dw_factor=dw_factor)
+        cross_sections = getCrossSections(Comments_dict)
     return laz
 
 
@@ -53,27 +66,21 @@ def getCrossSections(comments):
     # sigma_coh, sigma_inc, sigma_abs, nb_atoms
     # and use those to compute the total cross sections
     # It only works for single specie cases
-    
+
     # get nb_atoms
-    s = [c for c in comments if c.startswith('# nb_atoms')]
-    if not s: 
-        raise IOError("Comments do not contain number of atoms in unit cell")
-    if len(s) > 1:
-        raise IOError("Comments contain more than one linesfor nb_atoms. Confused")
-    nb_atoms = int(s[0][2:].strip().split()[1])
-    
+    nb_atoms = int(comments['nb_atoms'].strip().split()[2])
     # sigma
-    signature = '# sigma'
-    sigma_comments = [c for c in comments if c.startswith(signature)]
-    assert len(sigma_comments) == 3
+    sigma_list = filter(lambda x: x.startswith('sigma'),comments.keys())
+    assert len(sigma_list) == 3
     class xs: pass
-    for l in sigma_comments:
+    for sig_l in sigma_list:
+        l=comments[sig_l]
         l = l[2:].strip()
         tokens = l.split()
         name = tokens[0]
         value = tokens[1]
         comment = ' '.join(tokens[2:])
-        print name, value, comment
+        print(name, value, comment)
         assert comment[-6:] == '[barn]'
         setattr(xs, name[6:], float(value) * nb_atoms)
         continue
@@ -81,27 +88,42 @@ def getCrossSections(comments):
     return xs
 
 
-def getPeaks(text, lat, line_width=0, dw_factor=1):
+def getPeaks(text, comments, lat, line_width=0, dw_factor=1):
+    """
+    Parse the peaks information from the laz file.
+    """
+    # figure out columns
+    column_list = filter(lambda x: x.startswith('column'),comments.keys())
     # peaks records
     peaks = []
-    
+
+
     p       = re.compile(COMMENT, re.DOTALL)    # Remove comments
     s       = re.sub(p, '', text)
     lines   = s.split("\n")
-    # XXX: positions are hard-coded!!!
-    for l in lines:
-        l       = l.strip()
-        if not l:     # Empty line
+    col_idx={}
+    for col in column_list:
+        #print('\n\n line:'+comments[col]+'\n\n')
+        col_idx[col] = int(comments[col].split()[2]) - 1
+
+    for lin in lines:
+        lin = lin.strip()
+        if not lin:     # Empty line
             continue
-        row     = l.split()
-        peak    = {}
-        h       = int(row[0])
-        k       = int(row[1])
-        l       = int(row[2])
-        F       = float(row[12])      # F factor
-        mult    = int(row[16])    # Multiplicity
+        row = lin.split()
+        peak = {}
+        # this assumes integer hkl is there a case for non integer?
+        h       = int(row[col_idx['column_h']])
+        k       = int(row[col_idx['column_k']])
+        l       = int(row[col_idx['column_l']])
+        if 'column_F2' in column_list:
+            F2 = float(row[col_idx['column_F2']])
+        else:
+            F = float(row[col_idx['column_F']])
+            F2 = F*F
+        mult    = int(row[col_idx['column_j']])    # Multiplicity
         q       = _q(lat, h, k, l)
-        F2      = F*F
+
         peak    = Peak(
             q = q, F_squared = F2,
             multiplicity = mult,
@@ -110,7 +132,7 @@ def getPeaks(text, lat, line_width=0, dw_factor=1):
             )
         peaks.append(peak)
         continue
-    
+
     # sort peaks by q
     peaks = sorted(peaks, key=lambda p: p.q)
     return peaks
@@ -126,9 +148,8 @@ def _q(lattice, h, k, l):
 
 
 def getLattice(lines):
-    # !!! This demands that 2nd line contains lattice parameters
-    cell    = lines[1]      # Second lines should have lattice parameters
-    assert cell.startswith('# CELL')
+    cell    = lines
+    #assert cell.startswith('# CELL')
     cell    = cell.replace("#", "")
     par     = cell.split()
 
@@ -144,4 +165,4 @@ def getLattice(lines):
 # version
 __id__ = "$Id$"
 
-# End of file 
+# End of file
