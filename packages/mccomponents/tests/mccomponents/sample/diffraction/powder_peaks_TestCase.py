874eb6b1934c00343722b5acd66a9e73ac668024
Jiao Lin <linjiao@ornl.gov>
2018-05-07 23:18:05 -0400

    Refs #303. adding a test case

---
+++ packages/mccomponents/tests/mccomponents/sample/diffraction/powder_peaks_TestCase.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import numpy as np
+from mccomponents.sample.diffraction import powderpeaks
+from danse.ins import matter
+
+from fccNi import fccNi
+
+
+def test_F_i():
+    assert np.isclose(powderpeaks.F_i(0, fccNi, (1,1,1), 300), 10.11, rtol=0.002)
+    return
+
+def test_F():
+    assert np.isclose(powderpeaks.F(fccNi, (1,1,1), 300), 40.44, rtol=0.002)
+    assert np.isclose(powderpeaks.F(fccNi, (2,0,0), 300), 40.19, rtol=0.002)
+    assert np.isclose(powderpeaks.F(fccNi, (4,4,4), 300), 30.6, rtol=0.002)
+    return
+
+def test_d():
+    assert np.isclose(powderpeaks.d(fccNi.lattice, (1,1,1)), 2.03447)
+    return
+
+def test_peaks():
+    for pk in powderpeaks.iter_peaks(fccNi, 300):
+        print pk
+    return
+
+
+def main():
+    test_F_i()
+    test_F()
+    test_d()
+    test_peaks()
+    return
+
+
+if __name__ == '__main__': main()
+
+
+# End of file
