dd8302d8ffbc45fb05a1e7c660798b0c29bbd17e
Jiao Lin <linjiao@ornl.gov>
2018-05-07 23:18:05 -0400

    Refs #303. adding a test case

---
+++ packages/mccomponents/tests/mccomponents/sample/diffraction/fccNi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from danse.ins import matter
+
+atoms = [matter.Atom('Ni', (0,0,0)), matter.Atom('Ni', (0.5, 0.5, 0)),
+         matter.Atom('Ni', (0.5,0,0.5)), matter.Atom('Ni', (0, 0.5, 0.5))]
+a=3.5238
+alpha = 90.
+lattice = matter.Lattice(a=a, b=a, c=a, alpha=alpha, beta=alpha, gamma=alpha)
+fccNi = matter.Structure(atoms, lattice, sgid=225)
+
+# End of file
