1bf043f19985cca7f3444bb03e828074ce918a12
Jiao Lin <linjiao@ornl.gov>
2018-08-18 06:57:18 -0400

    Refs #324. improved impelementation by first computing the intersections with shape1

--- packages/mccomposite/lib/geometry/overlap.cc
+++ packages/mccomposite/lib/geometry/overlap.cc
@@ -1,5 +1,7 @@
 #include <cstdlib>
+#include <cmath>
 #include "mccomposite/geometry/locate.h"
+#include "mccomposite/geometry/intersect.h"
 #include "mccomposite/geometry/overlap.h"
 #include "journal/debug.h"
 #include "mccomposite/geometry/shape2ostream.h"
@@ -20,17 +22,36 @@ bool mccomposite::geometry::hasOverlap
  size_t N
  )
 {
-  // randomly generate points in bounding box, and make sure they are not
-  // inside or on surface both shapes
+  // randomly calculate intersections of a ray with a
+  // shape. The intersections must be on the surface of that shape.
+  // if that point is on the surface or the inside of the other shape
+  // then we have a overlap
+  
   for (size_t i=0; i<N; i++) {
+    // std::cout << "iteration " << i << std::endl;
     double x = random(bb.cx-bb.sx/2, bb.cx+bb.sx/2);
     double y = random(bb.cy-bb.sy/2, bb.cy+bb.sy/2);
     double z = random(bb.cz-bb.sz/2, bb.cz+bb.sz/2);
-    Position p(x,y,z);
-    Locator::Location l1 = locate(p, shape1);
-    Locator::Location l2 = locate(p, shape2);
-    if (l1!=Locator::outside &&l2!=Locator::outside) {
-      return true;
+    Position start(x,y,z);
+
+    double cos_theta = random(-1., 1.);
+    double sin_theta = std::sqrt(1-cos_theta*cos_theta);
+    double phi = random(0, 2*M_PI);
+    Direction dir(sin_theta*std::cos(phi), sin_theta*std::sin(phi), cos_theta);
+
+    Arrow arrow(start, dir);
+    // std::cout << start << ", " << dir << std::endl;
+
+    ArrowIntersector::distances_t ds = intersect(arrow, shape1);
+    // std::cout << "ds size" << ds.size() << std::endl;
+    for (int i_p=0; i_p<ds.size(); i_p++) {
+      // std::cout << "intersection #" << i_p << std::endl;
+      Position p = start + ds[i_p]*dir;
+      Locator::Location l2 = locate(p, shape2);
+      // std::cout << p << ", " << l2 << std::endl;
+      if (l2!=Locator::outside) {
+	return true;
+      }
     }
   }
   return false;
--- packages/mccomposite/tests/libmccomposite/geometry/testOverlap.cc
+++ packages/mccomposite/tests/libmccomposite/geometry/testOverlap.cc
@@ -10,6 +10,7 @@
 
 
 using namespace mccomposite::geometry;
+size_t N = 100;
 
 void test1()
 {
@@ -22,34 +23,59 @@ void test1()
   // shifted along z
   Translation tbox1(box2, Vector(0,0,2.999));
   BoundingBox bb1={0,0,1.5,1,2,6};
-  assert(hasOverlap(box1, tbox1, bb1, 10000000));
+  assert(hasOverlap(box1, tbox1, bb1, N));
 
   Translation tbox1a(box2, Vector(0,0,3.001));
-  assert(!hasOverlap(box1, tbox1a, bb1, 10000000));
+  assert(!hasOverlap(box1, tbox1a, bb1, N));
+
+  Translation tbox1b(box2, Vector(0,0,3.));
+  assert(hasOverlap(box1, tbox1b, bb1, N));
 
   // shifted along y
   Translation tbox2(box2, Vector(0,1.999,0));
   BoundingBox bb2={0,1,0,1,4,3};
-  assert(hasOverlap(box1, tbox2, bb2, 10000000));
+  assert(hasOverlap(box1, tbox2, bb2, N));
 
   Translation tbox2a(box2, Vector(0,2.001,0));
-  assert(!hasOverlap(box1, tbox2a, bb2, 10000000));
+  assert(!hasOverlap(box1, tbox2a, bb2, N));
 }
 
 void test2()
 {
-  Cylinder outcyl1(3, 3), incyl1(2, 3);
-  Cylinder outcyl2(2.001, 3), incyl2(1, 3);
+  Cylinder outcyl1(3, 3), incyl1(2, 5);
+  Cylinder outcyl2(2, 3), incyl2(1, 5);
   Difference hcyl1(outcyl1, incyl1);
   Difference hcyl2(outcyl2, incyl2);
   BoundingBox bb={0,0,0, 6,6,3};
-  assert(hasOverlap(hcyl1, hcyl2, bb, 10000000));
+  assert(hasOverlap(hcyl1, hcyl2, bb, N));
+}
+
+void test2a()
+{
+  Cylinder outcyl1(3, 3), incyl1(2, 5);
+  Cylinder outcyl2(1.9999, 3), incyl2(1, 5);
+  Difference hcyl1(outcyl1, incyl1);
+  Difference hcyl2(outcyl2, incyl2);
+  BoundingBox bb={0,0,0, 6,6,3};
+  assert(!hasOverlap(hcyl1, hcyl2, bb, N));
+}
+
+void test2b()
+{
+  Cylinder outcyl1(3, 3), incyl1(2, 5);
+  Cylinder outcyl2(2.000001, 3), incyl2(1, 5);
+  Difference hcyl1(outcyl1, incyl1);
+  Difference hcyl2(outcyl2, incyl2);
+  BoundingBox bb={0,0,0, 6,6,3};
+  assert(hasOverlap(hcyl1, hcyl2, bb, N));
 }
 
 int main()
 {
   test1();
   test2();
+  test2a();
+  test2b();
 }
 
 // End of file 
