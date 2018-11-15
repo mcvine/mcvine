b6c532deab0816e4bf1f11a6097132ab1d9bc62f
Jiao Lin <linjiao@ornl.gov>
2018-11-14 21:38:57 -0500

    Refs #333. implemented CLI `mcvine sampleassembly check`

--- packages/mcvine/python/mcvine/cli/sampleassembly/check.py
+++ packages/mcvine/python/mcvine/cli/sampleassembly/check.py
@@ -4,6 +4,7 @@
 #
 
 import os, click
+import warnings
 
 from . import sampleassembly
 @sampleassembly.command()
@@ -13,6 +14,15 @@ def check(xml):
     from mccomponents.sample import samplecomponent
     c = samplecomponent("s", xml)
     c.checkShapeOverlap()
+    # check multiple scattering setting
+    import  mcni
+    neutron = mcni.neutron(r=(0,0,0), v=(0,0,1))
+    from mccomposite import mccompositebp
+    nsegments = mccompositebp.countIntersections(neutron, c.shape())/2
+    if c.max_multiplescattering_loops_among_scatterers < nsegments:
+        warnings.warn("For multiple-scattering, `max_multiplescattering_loops_among_scatterers` should be larger than %s." % nsegments)
+    if c.min_neutron_probability == 0:
+        warnings.warn("Please set `min_neutron_probability` to a positive value to avoid excessive multiple-scattering loops")
     return
 
 
