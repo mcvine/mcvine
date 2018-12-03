751a9fe5bce7254c0ccd8d66ba64c66076df9a4e
Jiao Lin <linjiao@ornl.gov>
2018-12-02 22:59:37 -0500

    Refs #325. Implemented a parallel version of run_ppsd in mcni; modified mcvine.run_script to use run_ppsd in parallel; implemented parallel version of file merging in  HistogramBasedMonitorMixin; modified a test case to test HistogramBasedMonitorMixin

--- packages/mcni/python/mcni/components/HistogramBasedMonitorMixin.py
+++ packages/mcni/python/mcni/components/HistogramBasedMonitorMixin.py
@@ -29,13 +29,29 @@ e.g.
 number_mc_samples_filename = 'number_of_mc_samples'
 
 def merge_and_normalize(histogramfilename, outdir):
-    h, n = hist_mcs_sum(outdir, histogramfilename)
+    h, n = hist_mcs_sum_parallel(outdir, histogramfilename)
     h.I/=n
     h.E2/=n*n
     import os
     p = os.path.join(outdir, histogramfilename)
     from histogram.hdf import dump
     dump(h, p, '/', 'c')
+
+    import os
+    if not os.environ.get('MCVINE_DEBUG_PARALLEL_PPS'): return
+    from mpi4py import MPI
+    world = MPI.COMM_WORLD
+    rank = world.Get_rank()
+    if rank == 0:
+        h, n = hist_mcs_sum(outdir, histogramfilename)
+        h.I/=n
+        h.E2/=n*n
+        import os
+        p = os.path.join(outdir, histogramfilename)
+        from histogram.hdf import dump
+        p0, ext = os.path.splitext(p)
+        p1 = p0+'-serial'+ext
+        dump(h, p1, '/', 'c')
     return
 
 
@@ -55,25 +71,88 @@ def hist_mcs_sum(outdir, histogramfilename):
     from histogram.hdf.utils import getOnlyEntry
     loadhist = lambda f: load(f, getOnlyEntry(f))
     h1 = loadhist(histfiles[0])
-
+    
     # XXX
     # due to a bug in h5py, we have to manually make a copy of the histogram
     # see http://code.google.com/p/h5py/issues/detail?id=121
     import histogram
     h1 = histogram.histogram(h1.name(), h1.axes(), h1.I, h1.E2)
     # XXX
-
+    
     def _addhistfile(f):
         h = loadhist(f)
         h1.I += h.I
         h1.E2 += h.E2
         return
     map(_addhistfile, histfiles[1:])
-
+    
     # load number_of_mc_samples
     loadmcs = lambda f: float(open(f).read())
     mcs = map(loadmcs, mcsamplesfiles)
     return h1, sum(mcs)
+
+
+def hist_mcs_sum_parallel(outdir, histogramfilename):
+    """compute the summed histogram and summed number of mc samples"""
+    from mpi4py import MPI
+    world = MPI.COMM_WORLD
+    rank = world.Get_rank()
+    size = world.Get_size()
+
+    # get file lists from node 0
+    if rank==0:
+        import glob, os
+        pattern = os.path.join(outdir, '*', histogramfilename)
+        histfiles = glob.glob(pattern)
+        pattern = os.path.join(outdir, '*', number_mc_samples_filename)
+        mcsamplesfiles = glob.glob(pattern)
+        assert len(histfiles) == len(mcsamplesfiles), "histogram files %s does not match #mcsample files %s" %(len(histfiles), len(mcsamplesfiles))
+    else:
+        histfiles, mcsamplesfiles = None, None
+    # broadcast to all nodes
+    histfiles, mcsamplesfiles = world.bcast((histfiles, mcsamplesfiles), root=0)
+    # got nothing. exit
+    if not histfiles:
+        return None, None
+
+    # divide the jobs
+    N = len(histfiles)
+    npernode = (N-1)//world.size + 1
+    sl = slice(npernode*world.rank, npernode*(world.rank+1))
+    histfiles1 = histfiles[sl]
+    # load the first one
+    import histogram.hdf as hh
+    h1 = hh.load(histfiles1[0])
+    # remained files
+    remained = histfiles1[1:]
+    for f in remained:
+        h = hh.load(f)
+        h1.I += h.I
+        h1.E2 += h.E2
+        continue
+    # load number_of_mc_samples
+    loadmcs = lambda f: float(open(f).read())
+    mcs = map(loadmcs, mcsamplesfiles[sl])
+    mcs = sum(mcs)
+    # merge results from all nodes
+    if world.rank==0:
+        h = h1
+        import numpy as np
+        buffer = np.empty(h1.shape(), dtype=h1.I.dtype)
+        for rank in range(1, world.size):
+            world.Recv(buffer, rank, tag=2018120101)
+            h.I += buffer
+            world.Recv(buffer, rank, tag=2018120102)
+            h.E2 += buffer
+            mcs += world.recv(source=rank, tag=2018120103)
+            continue
+    else:
+        world.Send(h1.I, 0, tag=2018120101)
+        world.Send(h1.E2, 0, tag=2018120102)
+        world.send(mcs, 0, tag=2018120103)
+    # wait for everybody to synchronize _here_
+    world.Barrier()
+    return h1, mcs
 
 
 from outputs import mcs_sum
