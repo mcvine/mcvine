cc8a10f1e4c57b3762ab189a00f871992a21a034
Jiao Lin <linjiao@ornl.gov>
2019-02-08 09:30:54 -0500

    Refs #367. trivial: renaming output dirs in mcvine/run_script/instrument_TestCase.py

--- packages/mcvine/tests/mcvine/run_script/instrument_TestCase.py
+++ packages/mcvine/tests/mcvine/run_script/instrument_TestCase.py
@@ -14,7 +14,7 @@ class TestCase(unittest.TestCase):
 
 
     def test1(self):
-        outdir = 'out-run1'
+        outdir = 'out-instrument-run1'
         if os.path.exists(outdir):
             shutil.rmtree(outdir)
         mcvine.run_script.run1('test_instrument.py', outdir, 1e5, overwrite_datafiles=False)
@@ -25,7 +25,7 @@ class TestCase(unittest.TestCase):
 
     
     def test2(self):
-        outdir = 'out-run1_mpi'
+        outdir = 'out-instrument-run1_mpi'
         if os.path.exists(outdir):
             shutil.rmtree(outdir)
         mcvine.run_script.run1_mpi('test_instrument.py', outdir, 1e5, overwrite_datafiles=False)
@@ -36,12 +36,12 @@ class TestCase(unittest.TestCase):
 
     
     def test3(self):
-        outdir = 'out-run_mpi'
+        outdir = 'out-instrument-run_mpi'
         if os.path.exists(outdir):
             shutil.rmtree(outdir)
         mcvine.run_script.run_mpi('test_instrument.py', outdir, 1e5, nodes=2, overwrite_datafiles=False)
-        h1 = hh.load('./out-run_mpi/IE.h5')
-        h2 = hh.load('./out-run_mpi/IE-serial.h5')
+        h1 = hh.load('./out-instrument-run_mpi/IE.h5')
+        h2 = hh.load('./out-instrument-run_mpi/IE-serial.h5')
         self.assertEqual(h1.axisNameList(), h2.axisNameList())
         self.assert_(np.allclose(h1.energy, h2.energy))
         self.assert_(np.allclose(h1.I, h2.I))
