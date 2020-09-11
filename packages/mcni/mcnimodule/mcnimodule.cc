6dd6c21a4fe7b76ac886373855106ac82b84dcb8
Jiao Lin <linjiao@caltech.edu>
2020-09-11 08:12:20 -0400

    mcnimodule.cc: migrate initmodule

--- packages/mcni/mcnimodule/mcnimodule.cc
+++ packages/mcni/mcnimodule/mcnimodule.cc
@@ -12,6 +12,21 @@
 // 
 
 #include <Python.h>
+#if PY_MAJOR_VERSION >= 3
+  #define MOD_ERROR_VAL NULL
+  #define MOD_SUCCESS_VAL(val) val
+  #define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
+  #define MOD_DEF(ob, name, doc, methods) \
+          static struct PyModuleDef moduledef = { \
+            PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
+          ob = PyModule_Create(&moduledef);
+#else
+  #define MOD_ERROR_VAL
+  #define MOD_SUCCESS_VAL(val)
+  #define MOD_INIT(name) void init##name(void)
+  #define MOD_DEF(ob, name, doc, methods) \
+          ob = Py_InitModule3(name, methods, doc);
+#endif
 
 #include "exceptions.h"
 #include "bindings.h"
@@ -22,13 +37,13 @@ char pymcni_module__doc__[] = "";
 
 // Initialization function for the module (*must* be called initmcni)
 extern "C"
-void
-initmcni()
+MOD_INIT(mcni)
 {
     // create the module and add the functions
-    PyObject * m = Py_InitModule4(
-        "mcni", pymcni_methods,
-        pymcni_module__doc__, 0, PYTHON_API_VERSION);
+    PyObject * m;
+    MOD_DEF(m, "mcni", pymcni_module__doc__, pymcni_methods)
+    if (m == NULL)
+        return MOD_ERROR_VAL;
 
     // get its dictionary
     PyObject * d = PyModule_GetDict(m);
@@ -44,7 +59,7 @@ initmcni()
 
     // register bp-void ptr converters
     mcnimodule::register_bp_voidptr_converters();
-    return;
+    return MOD_SUCCESS_VAL(m);
 }
 
 // version
