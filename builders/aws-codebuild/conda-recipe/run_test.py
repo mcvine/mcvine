9e3f243b1454c5571095287f5aab8dff1dca99ac
Jiao Lin <jiao.lin@gmail.com>
2020-09-16 14:27:57 -0400

    aws codebuild: 2 cores for test?

--- builders/aws-codebuild/conda-recipe/run_test.py
+++ builders/aws-codebuild/conda-recipe/run_test.py
@@ -12,7 +12,6 @@ The tests will run at a separate work directory.
 """
 
 import os
-os.environ['CORES']='2'
-cmd = 'mcvine test'
+cmd = 'CORES=2 mcvine test'
 if os.system(cmd):
     raise RuntimeError("%s failed" % cmd)
