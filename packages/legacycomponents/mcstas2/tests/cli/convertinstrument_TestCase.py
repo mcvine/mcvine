19424464bad6463b7b25b88e7cab2423eedb6627
Jiao Lin <jiao.lin@gmail.com>
2020-09-15 22:04:29 -0400

    make it work for 2 and 3 in mcstas2/tests/cli/convertinstrumen_TestCase.py

--- packages/legacycomponents/mcstas2/tests/cli/convertinstrument_TestCase.py
+++ packages/legacycomponents/mcstas2/tests/cli/convertinstrument_TestCase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 
-import os, unittest
+import os, unittest, json
 
 here = os.path.abspath(os.path.dirname(__file__) or '.')
 
@@ -23,10 +23,22 @@ class mcstas_parser_TestCase(unittest.TestCase):
         instr = os.path.join(here, 'simple.instr')
         import tempfile
         d = tempfile.mkdtemp(dir=here, prefix='tmp_convertinstrument')
+        # non pyre
         cmd = 'cd %s; mcvine mcstas convertinstrument %s --type=pyre' % (d, instr)
         self.assertTrue(os.system(cmd)==0)
-        cmd = 'diff expected/pyre %s' % d
-        self.assertTrue(os.system(cmd)==0)
+        # pyre
+        for fn in os.listdir('expected/pyre'):
+            if fn.startswith('.'): continue
+            p = os.path.join('expected/pyre', fn)
+            if fn.endswith('.json'):
+                with open(p) as stream:
+                    expected_data = json.load(stream)
+                with open(os.path.join(d, fn)) as stream:
+                    data = json.load(stream)
+                self.assertEqual(data, expected_data)
+            else:
+                cmd = 'diff expected/pyre/%s %s/%s' % (fn, d, fn)
+                self.assertEqual(os.system(cmd), 0)
         import shutil
         shutil.rmtree(d)
         return
