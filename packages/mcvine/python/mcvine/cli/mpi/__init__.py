51ed7b5fe7bccb3bf5cc3c26519a3e5745129521
Jiao Lin <jiao.lin@gmail.com>
2020-09-25 01:04:09 -0400

    added cmd 'mcvine mpi launcher'

--- packages/mcvine/python/mcvine/cli/mpi/__init__.py
+++ packages/mcvine/python/mcvine/cli/mpi/__init__.py
@@ -4,10 +4,17 @@
 # Jiao Lin <jiao.lin@gmail.com>
 #
 
+from __future__ import print_function
 from .. import mcvine
 
 @mcvine.group()
 def mpi():
+    return
+
+@mpi.command()
+def launcher():
+    from mcni.pyre_support.MpiApplication import mpi_launcher_choice as l
+    print(l)
     return
 
 from . import info
