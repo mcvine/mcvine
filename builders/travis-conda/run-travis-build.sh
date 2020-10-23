bc2407f506b1750feb800e31ab74f0b011d1223a
Jiao Lin <jiao.lin@gmail.com>
2020-10-23 12:43:10 -0400

    refactoring travis configuration/script

---
+++ builders/travis-conda/run-travis-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd `basename "$0"`
+# cd builders/travis-conda
+./create_meta_yaml $MCVINE_CONDA_PKG_VER $GIT_FULL_HASH
+grep version meta.yaml
+grep git_rev meta.yaml
+export ERRLOG=/home/travis/build/log.error
+echo "ERRLOG=" $ERRLOG
+export CORES=1
+conda build --python=$TRAVIS_PYTHON_VERSION .
