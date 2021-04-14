ed3c19ec38972da7e5aea8662ecbf401a30402bc
Jiao Lin <jiao.lin@gmail.com>
2021-04-13 21:08:15 -0700

    SQE_EnergyFocusing_kernel: suport absorption/scattering_cross_section

--- packages/mccomponents/python/mccomponents/sample/kernelxml/parser/SQE_EnergyFocusing_Kernel.py
+++ packages/mccomponents/python/mccomponents/sample/kernelxml/parser/SQE_EnergyFocusing_Kernel.py
@@ -11,6 +11,12 @@ class SQE_EnergyFocusing_Kernel(base):
     tag = "SQE_EnergyFocusing_Kernel"
 
     def createKernel( self, **kwds ):
+        absorption_cross_section = kwds.get('absorption_cross_section')
+        if absorption_cross_section:
+            absorption_cross_section = self._parse(absorption_cross_section)
+        scattering_cross_section = kwds.get('scattering_cross_section')
+        if scattering_cross_section:
+            scattering_cross_section = self._parse(scattering_cross_section)
         Qrange = self._parse( kwds['Q-range'] )
         Erange = self._parse( kwds['energy-range'] )
         Ef = self._parse( kwds['Ef'] )
@@ -20,6 +26,8 @@ class SQE_EnergyFocusing_Kernel(base):
         return ctor(
             Qrange = Qrange, Erange = Erange,
             Ef = Ef, dEf = dEf,
+            absorption_cross_section = absorption_cross_section,
+            scattering_cross_section = scattering_cross_section,
         )
 
 
