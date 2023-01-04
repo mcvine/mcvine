16be9840fc401fe91b59e60b0bb037d06c7a0257
Jiao Lin <jiao.lin@gmail.com>
2023-01-03 16:42:08 -0800

    cosmetic:   packages/mcni/python/mcni/_journal.py

--- packages/mcni/python/mcni/_journal.py
+++ packages/mcni/python/mcni/_journal.py
@@ -14,13 +14,13 @@
 """
 This module provides a wrapper of pyre journal.
 It allows developers to create logging that is fixed format
-(not configurable by user). 
+(not configurable by user).
 This is totally against the general idea of the design of journal,
 so use it only when you really need that functionality.
 Usually, journal formatting should be configurable from
 command line, config files, etc, like normal pyre thing.
 But in some cases, we do need to fix the formatting.
-Right now it is used in 
+Right now it is used in
 * mcni.pyre_support.Instrument
 * mcni.instrument_simulator.SimulationNode
 
@@ -31,17 +31,16 @@ class journal(object):
     @classmethod
     def logger(cls, type, name, **kwds):
         return _journal_logger(type, name, **kwds)
-        
+
 
 class _journal_logger(object):
-    
+
     def __init__(self, type, name, header=None, format=None, footer=None):
         self.type = type
         self.name = name
         self.header, self.format, self.footer = header, format, footer
         return
 
-    
     def __call__(self, msg):
         import journal
         r = journal.journal().device.renderer
@@ -51,7 +50,4 @@ class _journal_logger(object):
         r.header, r.footer, r.format = header, footer, format
         return
 
-# version
-__id__ = "$Id$"
-
-#  End of file 
+#  End of file
