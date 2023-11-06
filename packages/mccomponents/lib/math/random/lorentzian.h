bc98fe5e553d961dabf51b62da0be9ee55d11710
Jiao Lin <jiao.lin@gmail.com>
2023-11-05 21:06:14 -0800

    Refs #471 added c++ implementation

---
+++ packages/mccomponents/lib/math/random/lorentzian.h
@@ -0,0 +1,25 @@
+// -*- C++ -*-
+//
+// Jiao Lin <jiao.lin@gmail.com>
+//
+
+#ifndef MCCOMPONENTS_RANDOM_LORENTZIAN_H
+#define MCCOMPONENTS_RANDOM_LORENTZIAN_H
+
+
+namespace mccomponents{
+
+  namespace math{
+
+    // generate random numbers in a lorentzian distribution
+    // center at 0 and Gamma is 1
+    double lorentzian_distrib_rand();
+
+  } // math
+
+} // mccomponents
+
+
+#endif //MCCOMPONENTS_RANDOM_LORENTZIAN_H
+
+// End of file
---
+++ packages/mccomponents/tests/libmccomponents/kernels/sample/test_LorentzianBroadened_E_Q_Kernel.cc
@@ -0,0 +1,93 @@
+// -*- C++ -*-
+//
+// Jiao Lin <jiao.lin@gmail.com>
+//
+
+#include <iostream>
+#include <cassert>
+#include "mccomponents/kernels/sample/LorentzianBroadened_E_Q_Kernel.h"
+
+#define DEBUG
+
+#ifdef DEBUG
+#include "journal/debug.h"
+#endif
+
+struct E_Q {
+  double operator() (double Q) const
+  {
+    return Q*Q/3.;
+  }
+};
+
+struct Gamma_Q {
+  double operator() (double Q) const
+  {
+    return Q*0.5;
+  }
+};
+
+
+void test1()
+{
+  using namespace mccomponents::sample;
+  using namespace mccomponents::kernels;
+  namespace conversion = mcni::neutron_units_conversion;
+
+  typedef LorentzianBroadened_E_Q_Kernel<E_Q, IdentitySQ, Gamma_Q> kernel_t;
+  typedef mcni::Vector3<double> V3d;
+
+  double Qmin = 0., Qmax = 20;
+  double absorption_cross_section = 1.;
+  double scattering_cross_section = 1.;
+  kernel_t k
+    (
+     E_Q(),
+     IdentitySQ(),
+     Gamma_Q(),
+     Qmin, Qmax,
+     absorption_cross_section,
+     scattering_cross_section
+      );
+
+  using namespace mcni::Neutron;
+  Event ni
+    (State
+     ( State::position_t(0,0,0),
+       State::velocity_t(0,0,7750),
+       State::spin_t() ),
+     0, 1.);
+
+  V3d vi = ni.state.velocity;
+  double Ei = conversion::v2E(vi.length());
+
+  for (int i=0; i<10; i++) {
+    Event n = ni;
+    k.scatter(n);
+    V3d vf = n.state.velocity;
+    V3d vdiff = vi-vf;
+    double Q = conversion::v2k * vdiff.length();
+    double vfl = vf.length();
+    double Ef = conversion::v2E(vfl);
+    double E = Ei-Ef;
+#ifdef DEBUG
+    std::cout << "Q=" << Q << ", "
+	      << "E=" << E << ", "
+	      << "n=" << n << ", "
+	      << "dE=" << E-Q*Q/3.
+	      << std::endl;
+#endif
+  }
+}
+
+
+int main()
+{
+#ifdef DEBUG
+  journal::debug_t("LorentzianBroadened_E_Q_Kernel").activate();
+#endif
+  test1();
+  return 0;
+}
+
+// End of file
