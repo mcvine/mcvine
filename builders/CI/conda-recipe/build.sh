ec3f39498b3b023cc7d6d0df13413ab599270419
Daniel Caballero <dlcaballero16@gmail.com>
2025-07-17 16:27:52 -0400

    Added flags

--- builders/CI/conda-recipe/build.sh
+++ builders/CI/conda-recipe/build.sh
@@ -5,26 +5,31 @@ let CORES-=1
 if ((CORES < 1)); then
     CORES = 1;
 fi
-
+echo "HERE 1"
 PYVER_MAJOR=`python -c "from __future__ import print_function; import sys; print(sys.version_info[0])"`
 PYVER_MINOR=`python -c "from __future__ import print_function; import sys; print(sys.version_info[1])"`
 PYVER=${PYVER_MAJOR}.${PYVER_MINOR}
+echo "HERE 2"
 echo "PYVER" $PYVER
 echo "PREFIX" ${PREFIX}
+echo "HERE 3"
 PY_INCLUDE_DIR=${PREFIX}/include/`ls ${PREFIX}/include/|grep python${PYVER}`
 PY_SHAREDLIB=${PREFIX}/lib/`ls ${PREFIX}/lib/|grep libpython${PYVER}[a-z]*.so$`
 echo $PY_INCLUDE_DIR
 echo $PY_SHAREDLIB
+echo "HERE 4"
 
 mkdir build
 cd build
 cmake -DCONDA_BUILD=TRUE \
+      -DCONDA_NO_PLUGINS=true \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DDEPLOYMENT_PREFIX=$PREFIX \
       -DPYTHON_INCLUDE_DIR=${PY_INCLUDE_DIR} \
       -DPYTHON_LIBRARY=${PY_SHAREDLIB} \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib \
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
       .. \
     && make -j$CORES \
     && make -j$CORES reconfigure-to-include-mcstas-components \
