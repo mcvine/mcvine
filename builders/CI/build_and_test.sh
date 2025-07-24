db952479f5ed36179b108c09e30af1811526b726
Patrou, Maria <patroum@ornl.gov>
2025-07-24 16:07:41 -0400

    automatic creation removed

--- builders/CI/build_and_test.sh
+++ builders/CI/build_and_test.sh
@@ -3,8 +3,6 @@
 set -x
 set -e
 #
-export PATH=$HOME/mc/bin:$PATH
-#
 export GIT_FULL_HASH=`git rev-parse HEAD`
 export GIT_VER=`git describe --tags`
 export VERSION=`git describe --tags | cut -d '-' -f1 | cut -c2-`
@@ -14,10 +12,6 @@ export MCVINE_CONDA_PKG_VER=${VERSION_NEXT}.dev
 echo $MCVINE_CONDA_PKG_VER
 cd builders/CI/conda-recipe
 
-# create meta.yaml
-./create_meta_yaml $MCVINE_CONDA_PKG_VER $GIT_FULL_HASH
-grep version meta.yaml
-grep git_rev meta.yaml
 
 # build
 cat meta.yaml
