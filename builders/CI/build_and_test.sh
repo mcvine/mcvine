3e4598801e5ea81cb1173c5e2f790d12a9330944
Daniel Caballero <132241327+dlcaballero16@users.noreply.github.com>
2025-07-25 13:06:04 -0400

    Fix CI build issue and modernization (#485)
    * Attempt to fix issue with libgcc version
    
    * Added flags
    
    * Set flag in build and test script
    
    * Set flag elsewhere
    
    * micromamba in CI, channels update
    
    * activate environment
    
    * environment info
    
    * acticvate removed
    
    * change directory and micromamb commands
    
    * micromamba activate in CI step
    
    * micromamba init in powershell
    
    * micromamba path
    
    * micromamba exe path
    
    * bash shell
    
    * channel reordering
    
    * histogram upgrade back
    
    * tests and cmakelist flag update
    
    * pin python subversion and set in build step
    
    * revert subversion in python, ubutntu 22
    
    * update variables in cmakelists
    
    * no test option added
    
    * mcvine test separately with required project added
    
    * check with miniconda
    
    * micromamba later version
    
    * Removed create meta yaml script
    
    * automatic creation removed
    
    * channel type
    
    * get the latest commit hash with git log
    
    * get the latest commit hash with git log of the current branch
    
    * show current branch
    
    ---------
    
    Co-authored-by: Patrou, Maria <patroum@ornl.gov>

--- builders/CI/build_and_test.sh
+++ builders/CI/build_and_test.sh
@@ -2,11 +2,10 @@
 
 set -x
 set -e
-
 #
-export PATH=$HOME/mc/bin:$PATH
-#
-export GIT_FULL_HASH=`git rev-parse HEAD`
+CURRENT_BRANCH=$GITHUB_HEAD_REF
+echo "Current branch: $CURRENT_BRANCH"
+export GIT_FULL_HASH=`git log -1 --format=%H`
 export GIT_VER=`git describe --tags`
 export VERSION=`git describe --tags | cut -d '-' -f1 | cut -c2-`
 export VERSION_NEXT=`echo ${VERSION}| awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
@@ -15,14 +14,17 @@ export MCVINE_CONDA_PKG_VER=${VERSION_NEXT}.dev
 echo $MCVINE_CONDA_PKG_VER
 cd builders/CI/conda-recipe
 
-# create meta.yaml
-./create_meta_yaml $MCVINE_CONDA_PKG_VER $GIT_FULL_HASH
-grep version meta.yaml
-grep git_rev meta.yaml
-
 # build
 cat meta.yaml
 cat conda_build_config.yaml
 echo "Conda environment packages"
-conda list
-conda build .
+micromamba list
+cd ../
+pwd
+conda build --no-test .
+#install conda package
+conda install -y -c conda-forge conda-build conda-index
+python -m conda_index ${CONDA_PREFIX}/conda-bld/
+conda install -y -c ${CONDA_PREFIX}/conda-bld/ mcvine-core
+#test package
+mcvine test
--- builders/CI/conda-recipe/build.sh
+++ builders/CI/conda-recipe/build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-
 let CORES=`grep -c ^processor /proc/cpuinfo`
 let CORES-=1
 if ((CORES < 1)); then
     CORES = 1;
 fi
-
 PYVER_MAJOR=`python -c "from __future__ import print_function; import sys; print(sys.version_info[0])"`
 PYVER_MINOR=`python -c "from __future__ import print_function; import sys; print(sys.version_info[1])"`
 PYVER=${PYVER_MAJOR}.${PYVER_MINOR}
@@ -19,6 +17,7 @@ echo $PY_SHAREDLIB
 mkdir build
 cd build
 cmake -DCONDA_BUILD=TRUE \
+      -DCONDA_NO_PLUGINS=TRUE \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DDEPLOYMENT_PREFIX=$PREFIX \
       -DPYTHON_INCLUDE_DIR=${PY_INCLUDE_DIR} \
